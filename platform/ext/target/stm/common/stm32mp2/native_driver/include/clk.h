e8c0e2ecbed7a478204d3d2e63d84cf1764487b8
Ludovic Barre <ludovic.barre@st.com>
2020-11-16 11:51:11 +0100

    platform: stm32mp2: import clk_ops interface from tfa
    This patch import clk_ops interface from tfa.
    
    Change-Id: I997f5e395894e415f3a52b086201be7e968c0cc6
    Signed-off-by: Ludovic Barre <ludovic.barre@st.com>

---
+++ platform/ext/target/stm/common/stm32mp2/native_driver/include/clk.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2020, STMicroelectronics - All Rights Reserved
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef  __CLK_H__
+#define  __CLK_H__
+
+#include <stdbool.h>
+
+typedef struct clk_ops {
+	int (*enable)(unsigned long id);
+	void (*disable)(unsigned long id);
+	unsigned long (*get_rate)(unsigned long id);
+        int (*get_parent)(unsigned long id);
+	bool (*is_enabled)(unsigned long id);
+} clk_ops_t;
+
+void clk_enable(unsigned long id);
+void clk_disable(unsigned long id);
+unsigned long clk_get_rate(unsigned long id);
+bool clk_is_enabled(unsigned long id);
+int  clk_get_parent(unsigned long id);
+
+void clk_register(const clk_ops_t *ops);
+
+#endif   /* ----- #ifndef __CLK_H__  ----- */
