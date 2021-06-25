6e0f7ca52ba71e416b6558de5c303aaa06883911
Ludovic Barre <ludovic.barre@foss.st.com>
2021-06-25 17:39:26 +0200

    platform: stm32mp2: add stm32 log level
    This patch adds stm32 log level, can be set to:
    STM32_LOG_LEVEL_OFF
    STM32_LOG_LEVEL_ERROR
    STM32_LOG_LEVEL_WARNING
    STM32_LOG_LEVEL_INFO
    STM32_LOG_LEVEL_DEBUG
    
    Change-Id: Idc18754e8584e0729fbd4749b3ebe033761c2b61
    Signed-off-by: Ludovic Barre <ludovic.barre@foss.st.com>

---
+++ platform/ext/target/stm/common/stm32mp2/config.cmake
@@ -0,0 +1,8 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2021, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-------------------------------------------------------------------------------
+
+set(STM32_LOG_LEVEL		STM32_LOG_LEVEL_INFO	CACHE STRING	"Set default stm32 log level as NOTICE, (see debug.h)")
--- platform/ext/target/stm/common/stm32mp2/native_driver/include/debug.h
+++ platform/ext/target/stm/common/stm32mp2/native_driver/include/debug.h
@@ -8,93 +8,51 @@
 #define  DEBUG_H
 
 #include <lib/utils_def.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#define STM32_LOG_LEVEL_OFF       0
+#define STM32_LOG_LEVEL_ERROR     1
+#define STM32_LOG_LEVEL_WARNING   2
+#define STM32_LOG_LEVEL_INFO      3
+#define STM32_LOG_LEVEL_DEBUG     4
+
+#if STM32_LOG_LEVEL >= STM32_LOG_LEVEL_ERROR
+# define EMSG(_fmt, ...) printf("[ERR] " _fmt "\r\n", ##__VA_ARGS__)
+#else
+# define EMSG(...) (void)0
+#endif
+
+#if STM32_LOG_LEVEL >= STM32_LOG_LEVEL_WARNING
+# define WMSG(_fmt, ...) printf("[WRN] " _fmt "\r\n", ##__VA_ARGS__)
+#else
+# define WMSG(...) (void)0
+#endif
+
+#if STM32_LOG_LEVEL >= STM32_LOG_LEVEL_INFO
+# define IMSG(_fmt, ...) printf("[INF] " _fmt "\r\n", ##__VA_ARGS__)
+#else
+# define IMSG(...) (void)0
+#endif
+
+#if STM32_LOG_LEVEL >= STM32_LOG_LEVEL_DEBUG
+# define DMSG(_fmt, ...) printf("[DBG] " _fmt "\r\n", ##__VA_ARGS__)
+#else
+# define DMSG(...) (void)0
+#endif
 
 /* for compatibilities with optee drivers */
-#define EMSG ERROR
-#define IMSG INFO
-#define DMSG VERBOSE
-#define FMSG VERBOSE
+#define MSG  (void)0
+#define FMSG DMSG
 
-/*
- * The log output macros print output to the console. These macros produce
- * compiled log output only if the LOG_LEVEL defined in the makefile (or the
- * make command line) is greater or equal than the level required for that
- * type of log output.
- *
- * The format expected is the same as for printf(). For example:
- * INFO("Info %s.\n", "message")    -> INFO:    Info message.
- * WARN("Warning %s.\n", "message") -> WARNING: Warning message.
- */
-
-#define LOG_LEVEL_NONE			U(0)
-#define LOG_LEVEL_ERROR			U(10)
-#define LOG_LEVEL_NOTICE		U(20)
-#define LOG_LEVEL_WARNING		U(30)
-#define LOG_LEVEL_INFO			U(40)
-#define LOG_LEVEL_VERBOSE		U(50)
-
-#ifndef __ASSEMBLER__
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stdio.h>
-
-#define tf_log(fmt, ...) printf(fmt, ##__VA_ARGS__)
-
-/*
- * Define Log Markers corresponding to each log level which will
- * be embedded in the format string and is expected by tf_log() to determine
- * the log level.
- */
-#define LOG_MARKER_ERROR		"\xa"	/* 10 */
-#define LOG_MARKER_NOTICE		"\x14"	/* 20 */
-#define LOG_MARKER_WARNING		"\x1e"	/* 30 */
-#define LOG_MARKER_INFO			"\x28"	/* 40 */
-#define LOG_MARKER_VERBOSE		"\x32"	/* 50 */
-
-/*
- * If the log output is too low then this macro is used in place of tf_log()
- * below. The intent is to get the compiler to evaluate the function call for
- * type checking and format specifier correctness but let it optimize it out.
- */
-#define no_tf_log(fmt, ...)				\
-	do {						\
-		if (false) {				\
-			tf_log(fmt, ##__VA_ARGS__);	\
-		}					\
-	} while (false)
-
-#if LOG_LEVEL >= LOG_LEVEL_ERROR
-# define ERROR(...)	tf_log(LOG_MARKER_ERROR __VA_ARGS__)
-#else
-# define ERROR(...)	no_tf_log(LOG_MARKER_ERROR __VA_ARGS__)
-#endif
-
-#if LOG_LEVEL >= LOG_LEVEL_NOTICE
-# define NOTICE(...)	tf_log(LOG_MARKER_NOTICE __VA_ARGS__)
-#else
-# define NOTICE(...)	no_tf_log(LOG_MARKER_NOTICE __VA_ARGS__)
-#endif
-
-#if LOG_LEVEL >= LOG_LEVEL_WARNING
-# define WARN(...)	tf_log(LOG_MARKER_WARNING __VA_ARGS__)
-#else
-# define WARN(...)	no_tf_log(LOG_MARKER_WARNING __VA_ARGS__)
-#endif
-
-#if LOG_LEVEL >= LOG_LEVEL_INFO
-# define INFO(...)	tf_log(LOG_MARKER_INFO __VA_ARGS__)
-#else
-# define INFO(...)	no_tf_log(LOG_MARKER_INFO __VA_ARGS__)
-#endif
-
-#if LOG_LEVEL >= LOG_LEVEL_VERBOSE
-# define VERBOSE(...)	tf_log(LOG_MARKER_VERBOSE __VA_ARGS__)
-#else
-# define VERBOSE(...)	no_tf_log(LOG_MARKER_VERBOSE __VA_ARGS__)
-#endif
+/* for compatibilities with tfa drivers */
+#define ERROR	EMSG
+#define NOTICE	WMSG
+#define WARN	WMSG
+#define INFO	IMSG
+#define VERBOSE DMSG
 
 #define panic() while(1)
 
-#endif /* __ASSEMBLER__ */
 #endif /* DEBUG_H */
 
