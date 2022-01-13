9afe4d79501f02909c21f25f61193a55364bf244
Ludovic Barre <ludovic.barre@foss.st.com>
2022-01-13 13:30:25 +0100

    platform: stm: adaptation to Duplicate tfm_spm_hal_* APIs for IPC Model
    adaptation for:
    HAL: Duplicates three legacy tfm_spm_hal_* APIs for IPC Model
    
    This patch duplicates the following APIs for IPC Model in platform HAL
    and renames them:
    - tfm_spm_hal_get_ns_VTOR -> tfm_hal_get_ns_VTOR
    - tfm_spm_hal_get_ns_entry_point -> tfm_hal_get_ns_entry_point
    - tfm_spm_hal_get_ns_MSP -> tfm_hal_get_ns_MSP
    
    This original ones are kept for Library Model for now for easier
    potential deprecation of Library Model in the future.
    
    Change-Id: Ic6c459106153d4c9f2a0983eedaea9ef7277e3d7
    Signed-off-by: Ludovic Barre <ludovic.barre@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/248399

--- platform/ext/target/stm/common/stm32mp2/secure/tfm_platform_system.c
+++ platform/ext/target/stm/common/stm32mp2/secure/tfm_platform_system.c
@@ -8,10 +8,13 @@
 #include <tfm_platform_system.h>
 #include <tfm_hal_platform.h>
 #include <plat_device.h>
+#include <target_cfg.h>
 
 #include <uart_stdout.h>
 #include <stm32_icache.h>
 #include <stm32_dcache.h>
+
+extern const struct memory_region_limits memory_regions;
 
 void tfm_platform_hal_system_reset(void)
 {
@@ -48,4 +51,19 @@ enum tfm_hal_status_t tfm_hal_platform_init(void)
 	stdio_init();
 
 	return TFM_HAL_SUCCESS;
+}
+
+uint32_t tfm_hal_get_ns_VTOR(void)
+{
+    return memory_regions.non_secure_code_start;
+}
+
+uint32_t tfm_hal_get_ns_MSP(void)
+{
+    return *((uint32_t *)memory_regions.non_secure_code_start);
+}
+
+uint32_t tfm_hal_get_ns_entry_point(void)
+{
+    return *((uint32_t *)(memory_regions.non_secure_code_start + 4));
 }
