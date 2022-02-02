58ec94fad9fa8cef130a480a5254c7bb13ed6385
Ludovic Barre <ludovic.barre@foss.st.com>
2022-02-02 10:00:11 +0100

    platform: stm: split services and hal plat init for stm32mp2
    This patch splits services and hal plat init for stm32mp2
    
    Change-Id: Ib7327324c4e93d2afd8b5aafdf52c87fbe255bda
    Signed-off-by: Ludovic Barre <ludovic.barre@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/248401

--- platform/ext/target/stm/common/stm32mp2/secure/tfm_hal_platform.c
+++ platform/ext/target/stm/common/stm32mp2/secure/tfm_hal_platform.c
@@ -5,7 +5,6 @@
  *
  */
 #include <cmsis.h>
-#include <tfm_platform_system.h>
 #include <tfm_hal_platform.h>
 #include <plat_device.h>
 #include <target_cfg.h>
@@ -15,24 +14,6 @@
 #include <stm32_dcache.h>
 
 extern const struct memory_region_limits memory_regions;
-
-void tfm_platform_hal_system_reset(void)
-{
-	/* Reset the system */
-	NVIC_SystemReset();
-}
-
-enum tfm_platform_err_t tfm_platform_hal_ioctl(tfm_platform_ioctl_req_t request,
-		psa_invec  *in_vec,
-		psa_outvec *out_vec)
-{
-	(void)request;
-	(void)in_vec;
-	(void)out_vec;
-
-	/* Not needed for this platform */
-	return TFM_PLATFORM_ERR_NOT_SUPPORTED;
-}
 
 enum tfm_hal_status_t tfm_hal_platform_init(void)
 {
