86b46b088b1e2d0639593ad4a85c44c79a970d82
Ludovic Barre <ludovic.barre@foss.st.com>
2023-01-03 11:33:33 +0100

    platform: stm32mp2: spi: add OSPI driver
    This patch adds OSPI driver.
    
    Change-Id: Ib9ffd1a6a2c5df25d53d6354455dc60ba9129a2b
    Signed-off-by: Christophe Kerello <christophe.kerello@foss.st.com>
    Signed-off-by: Ludovic Barre <ludovic.barre@foss.st.com>

---
+++ platform/ext/target/stm/common/stm32mp2/native_driver/include/stm32_ospi.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021, STMicroelectronics - All Rights Reserved
+ *
+ * SPDX-License-Identifier: GPL-2.0+ OR BSD-3-Clause
+ */
+
+#ifndef STM32_OSPI_H
+#define STM32_OSPI_H
+
+#include <stm32_gpio.h>
+#include <stddef.h>
+
+struct stm32_ospi_platdata {
+	uintptr_t reg_base;
+	uintptr_t mm_base;
+	size_t mm_size;
+
+	unsigned long clock_id;
+	unsigned int reset_id;
+
+	struct pinctrl_cfg *pinctrl;
+
+	struct spi_slave *spi_slave;
+};
+
+int stm32_ospi_init(void);
+int stm32_ospi_deinit(void);
+
+#endif /* STM32_OSPI_H */
