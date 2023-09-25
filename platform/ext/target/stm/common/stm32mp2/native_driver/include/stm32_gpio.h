f6f454b51a274e5db3a64248c4e5dd9237b7815a
Ludovic Barre <ludovic.barre@foss.st.com>
2023-09-25 13:58:51 +0200

    platform: stm32mp2: update pinctrl gpio driver with devicetree support
    Change-Id: I9914043b1a22e828727e6b8f773f206e14b91635
    Signed-off-by: Ludovic Barre <ludovic.barre@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/330586
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>

--- platform/ext/target/stm/common/stm32mp2/native_driver/include/stm32_gpio.h
+++ platform/ext/target/stm/common/stm32mp2/native_driver/include/stm32_gpio.h
@@ -30,69 +30,16 @@
 #define GPIO_PULL_DOWN			0x02
 #define GPIO_PULL_MASK			U(0x03)
 
-#ifndef __ASSEMBLER__
 #include <stdint.h>
 
-/* client api */
-#define STM32_PINCFG(_pinsmux, _slew_rate, _pull, _open_drain) \
-	{                                                          \
-		.pinsmux = _pinsmux,                                   \
-		.npinsmux = ARRAY_SIZE(_pinsmux),                      \
-		.slew_rate = _slew_rate,                               \
-		.pull = _pull,                                         \
-		.open_drain = _open_drain,                             \
-	}
-
-struct pin_cfg {
-	uint32_t *pinsmux;
-	int npinsmux;
-	uint32_t slew_rate;
+struct gpio_cfg {
+	uint32_t bank;
+	uint32_t pin;
+	uint32_t mode;
+	uint32_t type;
+	uint32_t speed;
 	uint32_t pull;
-	uint32_t open_drain;
+	uint32_t alternate;
 };
-
-#define PINCTRL_CFG_DEFINE(_name, _pinctrl_cfg)            \
-	struct pinctrl_cfg _name = {                           \
-		.pins = _pinctrl_cfg,                              \
-	    .npins = ARRAY_SIZE(_pinctrl_cfg),                 \
-	};
-
-/**
- * struct pinctrl_cfg - Representation of pinctrl config
- *
- * @pins:	    reference on pins configuration
- * @npins:      number of pins cfg in this pinctrl config
- */
-struct pinctrl_cfg {
-	struct pin_cfg *pins;
-	int npins;
-};
-
-int set_pinctrl_config(struct pinctrl_cfg *pctrl_cfg);
-
-/* driver init */
-#define BANK_TO_ID(_bank) (_bank - 'A')
-
-#define STM32_BANK_CFG(_bank, _base, _clk_id) \
-	{                                         \
-		.id = BANK_TO_ID(_bank),              \
-		.base = _base,                        \
-		.clk_id = _clk_id,                    \
-	}
-
-struct bank_cfg {
-	uint32_t id;
-	unsigned long base;
-	uint32_t clk_id;
-};
-
-struct stm32_gpio_platdata {
-	const struct bank_cfg *banks;
-	int nbanks;
-};
-
-int stm32_gpio_init(void);
-
-#endif /*__ASSEMBLER__*/
 
 #endif /* STM32_GPIO_H */
