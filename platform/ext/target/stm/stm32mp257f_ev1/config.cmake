bfb536152f6d53dbd90b01cd5ab7045d1ce37d58
Ludovic Barre <ludovic.barre@foss.st.com>
2023-10-03 17:38:06 +0200

    platform: stm32mp257f_ev1: select ev1-revB devicetree
    Select ev1-revB device tree for main line build process.
    It's a temporary board (cut1.1), it will be removed before mass market.
    
    Change-Id: Ia660d9db712a1328e061e8f4ffddb76b3d778df7
    Signed-off-by: Ludovic Barre <ludovic.barre@foss.st.com>

--- platform/ext/target/stm/stm32mp257f_ev1/config.cmake
+++ platform/ext/target/stm/stm32mp257f_ev1/config.cmake
@@ -18,8 +18,8 @@ set(MBEDCRYPTO_BUILD_TYPE               minsizerel		CACHE STRING    "Build type 
 
 set(STM32_BOARD_MODEL	"stm32mp257f eval1"			CACHE STRING	  "Define board model name" FORCE)
 set(DTS_EXT_DIR		""					CACHE STRING	  "If not empty, set external dts directory")
-set(DTS_BOARD_S		"arm/stm/stm32mp257f-ev1_s.dts"		CACHE STRING	  "set board devicetree file")
-set(DTS_BOARD_NS	"arm/stm/stm32mp257f-ev1_ns.dts"	CACHE STRING	  "set board devicetree file")
+set(DTS_BOARD_S		"arm/stm/stm32mp257f-ev1-revB_s.dts"	CACHE STRING	  "set board devicetree file")
+set(DTS_BOARD_NS	"arm/stm/stm32mp257f-ev1-revB_ns.dts"	CACHE STRING	  "set board devicetree file")
 
 set(STM32_IPC				ON         CACHE BOOL     "Use IPC (rpmsg) to communicate with main processor" FORCE)
 set(BL2                                 OFF        CACHE BOOL     "Whether to build BL2" FORCE)
