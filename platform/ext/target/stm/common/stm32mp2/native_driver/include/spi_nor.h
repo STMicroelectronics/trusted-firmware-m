7b70c809503027d268fbd3579cec69c9ca7d344a
Christophe Kerello <christophe.kerello@foss.st.com>
2023-11-16 09:15:11 +0100

    platform: stm32mp2: spi-nor: add 4-bytes read/write/erase commands
    Add 4-bytes read/write/erase commands for Macronix memories.
    
    Change-Id: I6f414f404af54b847274b2b27f5483b1d639d56c
    Signed-off-by: Christophe Kerello <christophe.kerello@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/341390
    Domain-Review: Ludovic BARRE <ludovic.barre@foss.st.com>
    Tested-by: Christophe KERELLO <christophe.kerello@st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Christophe KERELLO <christophe.kerello@st.com>
    Reviewed-by: Ludovic BARRE <ludovic.barre@foss.st.com>

--- platform/ext/target/stm/common/stm32mp2/native_driver/include/spi_nor.h
+++ platform/ext/target/stm/common/stm32mp2/native_driver/include/spi_nor.h
@@ -10,41 +10,48 @@
 #include <spi_mem.h>
 
 /* OPCODE */
-#define SPI_NOR_OP_WREN		0x06U	/* Write enable */
-#define SPI_NOR_OP_WRDI		0x04U	/* Write disable */
-#define SPI_NOR_OP_WRSR		0x01U	/* Write status register 1 byte */
-#define SPI_NOR_OP_READ_ID	0x9FU	/* Read JEDEC ID */
-#define SPI_NOR_OP_READ_CR	0x35U	/* Read configuration register */
-#define SPI_NOR_OP_READ_SR	0x05U	/* Read status register */
-#define SPI_NOR_OP_READ_FSR	0x70U	/* Read flag status register */
-#define SPINOR_OP_RDEAR		0xC8U	/* Read Extended Address Register */
-#define SPINOR_OP_WREAR		0xC5U	/* Write Extended Address Register */
+#define SPI_NOR_OP_WREN			0x06U	/* Write enable */
+#define SPI_NOR_OP_WRDI			0x04U	/* Write disable */
+#define SPI_NOR_OP_WRSR			0x01U	/* Write status register 1 byte */
+#define SPI_NOR_OP_READ_ID		0x9FU	/* Read JEDEC ID */
+#define SPI_NOR_OP_READ_CR		0x35U	/* Read configuration register */
+#define SPI_NOR_OP_READ_SR		0x05U	/* Read status register */
+#define SPI_NOR_OP_READ_FSR		0x70U	/* Read flag status register */
+#define SPINOR_OP_RDEAR			0xC8U	/* Read Extended Address Register */
+#define SPINOR_OP_WREAR			0xC5U	/* Write Extended Address Register */
 
 /* Used for Spansion flashes only. */
-#define SPINOR_OP_BRWR		0x17U	/* Bank register write */
-#define SPINOR_OP_BRRD		0x16U	/* Bank register read */
+#define SPINOR_OP_BRWR			0x17U	/* Bank register write */
+#define SPINOR_OP_BRRD			0x16U	/* Bank register read */
 
-#define SPI_NOR_OP_READ		0x03U	/* Read data bytes (low frequency) */
-#define SPI_NOR_OP_READ_FAST	0x0BU	/* Read data bytes (high frequency) */
-#define SPI_NOR_OP_READ_1_1_2	0x3BU	/* Read data bytes (Dual Output SPI) */
-#define SPI_NOR_OP_READ_1_2_2	0xBBU	/* Read data bytes (Dual I/O SPI) */
-#define SPI_NOR_OP_READ_1_1_4	0x6BU	/* Read data bytes (Quad Output SPI) */
-#define SPI_NOR_OP_READ_1_4_4	0xEBU	/* Read data bytes (Quad I/O SPI) */
-#define SPI_NOR_OP_READ_1_1_8	0x8BU	/* Read data bytes (Octal Output SPI) */
-#define SPI_NOR_OP_READ_1_8_8	0xCBU	/* Read data bytes (Octal I/O SPI) */
+#define SPI_NOR_OP_READ			0x03U	/* Read data bytes (low frequency) */
+#define SPI_NOR_OP_READ_FAST		0x0BU	/* Read data bytes (high frequency) */
+#define SPI_NOR_OP_READ_1_1_2		0x3BU	/* Read data bytes (Dual Output SPI) */
+#define SPI_NOR_OP_READ_1_2_2		0xBBU	/* Read data bytes (Dual I/O SPI) */
+#define SPI_NOR_OP_READ_1_1_4		0x6BU	/* Read data bytes (Quad Output SPI) */
+#define SPI_NOR_OP_READ_1_4_4		0xEBU	/* Read data bytes (Quad I/O SPI) */
+#define SPI_NOR_OP_READ_1_1_8		0x8BU	/* Read data bytes (Octal Output SPI) */
+#define SPI_NOR_OP_READ_1_8_8		0xCBU	/* Read data bytes (Octal I/O SPI) */
 
-#define SPI_NOR_OP_WRITE	0x02U	/* Write data bytes */
-#define SPI_NOR_OP_WRITE_1_1_4	0x32U	/* Write data bytes (Quad page program) */
-#define SPI_NOR_OP_WRITE_1_4_4	0x38U	/* Write data bytes (Quad page program) */
-#define SPI_NOR_OP_WRITE_1_1_8	0x82U	/* Write data bytes (Octal page program) */
-#define SPI_NOR_OP_WRITE_1_8_8	0xC2U	/* Write data bytes (Octal page program) */
+#define SPI_NOR_OP_WRITE		0x02U	/* Write data bytes */
+#define SPI_NOR_OP_WRITE_1_1_4		0x32U	/* Write data bytes (Quad page program) */
+#define SPI_NOR_OP_WRITE_1_4_4		0x38U	/* Write data bytes (Quad page program) */
+#define SPI_NOR_OP_WRITE_1_1_8		0x82U	/* Write data bytes (Octal page program) */
+#define SPI_NOR_OP_WRITE_1_8_8		0xC2U	/* Write data bytes (Octal page program) */
 
-#define SPI_NOR_OP_SE		0x20U	/* Erase a sector */
-#define SPI_NOR_OP_BE		0xD8U	/* Erase a block */
+#define SPI_NOR_OP_SE			0x20U	/* Erase a sector */
+#define SPI_NOR_OP_BE			0xD8U	/* Erase a block */
+
+/* 4-bytes address opcodes */
+#define SPI_NOR_OP_READ_1_1_4_4B	0x6CU	/* Read data bytes (Quad Output SPI) */
+
+#define SPI_NOR_OP_WRITE_1_4_4_4B	0x3EU	/* Write data bytes (Quad page program) */
+
+#define SPI_NOR_OP_SE_4B		0x21U	/* Erase a sector */
 
 /* Flags for NOR specific configuration */
-#define SPI_NOR_USE_FSR		BIT(0)
-#define SPI_NOR_USE_BANK	BIT(1)
+#define SPI_NOR_USE_FSR			BIT(0)
+#define SPI_NOR_USE_BANK		BIT(1)
 
 struct nor_device {
 	struct spi_mem_op read_op;
