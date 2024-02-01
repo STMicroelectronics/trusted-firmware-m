5f331d321770b08d8177609c418abbd25706dbfa
Thomas Bourgoin <thomas.bourgoin@foss.st.com>
2024-02-01 18:26:45 +0100

    platform: stm32mp2: reset : add function _is_valid_dev()
    Factorise rstc test in function _is_valid_dev.
    Return -ENOTSUP if the device does not provide an API.
    Return -ENODEV if rstc is NULL or device is not ready.
    
    Change-Id: I9e46c84694d941c78acc8dce2cd7ed78e5fef9a1
    Signed-off-by: Thomas Bourgoin <thomas.bourgoin@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/356564
    Domain-Review: Ludovic BARRE <ludovic.barre@foss.st.com>
    Reviewed-by: Ludovic BARRE <ludovic.barre@foss.st.com>
    Tested-by: Ludovic BARRE <ludovic.barre@foss.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>

--- platform/ext/target/stm/common/stm32mp2/native_driver/src/reset/core.c
+++ platform/ext/target/stm/common/stm32mp2/native_driver/src/reset/core.c
@@ -6,6 +6,17 @@
 
 #include <reset.h>
 
+static inline int _is_valid_dev(const struct reset_control *rstc)
+{
+	if(!rstc || !device_is_ready(rstc->dev))
+		return -ENODEV;
+
+	if (!rstc->dev->api)
+		return -ENOTSUP;
+
+	return 0;
+}
+
 static inline int _status(const struct device *dev, uint32_t id)
 {
 	const struct reset_driver_api *api = dev->api;
@@ -18,8 +29,10 @@ static inline int _status(const struct device *dev, uint32_t id)
 
 int reset_control_status(struct reset_control *rstc)
 {
-	if (!rstc || !rstc->dev)
-		return -ENODEV;
+	int dev_err = _is_valid_dev(rstc);
+
+	if (dev_err)
+		return dev_err;
 
 	return _status(rstc->dev, rstc->id);
 }
@@ -36,8 +49,10 @@ static inline int _assert(const struct device *dev, uint32_t id)
 
 int reset_control_assert(struct reset_control *rstc)
 {
-	if (!rstc || !rstc->dev)
-		return -ENODEV;
+	int dev_err = _is_valid_dev(rstc);
+
+	if (dev_err)
+		return dev_err;
 
 	return _assert(rstc->dev, rstc->id);
 }
@@ -54,8 +69,10 @@ static inline int _deassert(const struct device *dev, uint32_t id)
 
 int reset_control_deassert(struct reset_control *rstc)
 {
-	if (!rstc || !rstc->dev)
-		return -ENODEV;
+	int dev_err = _is_valid_dev(rstc);
+
+	if (dev_err)
+		return dev_err;
 
 	return _deassert(rstc->dev, rstc->id);
 }
@@ -73,8 +90,10 @@ static inline int _reset(const struct device *dev, uint32_t id)
 
 int reset_control_reset(const struct reset_control *rstc)
 {
-	if (!rstc || !rstc->dev)
-		return -ENODEV;
+	int dev_err = _is_valid_dev(rstc);
+
+	if (dev_err)
+		return dev_err;
 
 	return _reset(rstc->dev, rstc->id);
 }
