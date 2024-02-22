2aa8a77f027ae49d40d030b1a7056588b54c0d78
Ludovic Barre <ludovic.barre@foss.st.com>
2024-02-22 16:46:02 +0100

    platform: stm32mp2: update uboot flash nor layout
    Update uboot flash nor layout
    
    Change-Id: I57e7d14d8e651d1a8c6ef34a9396024df6f1cf60
    Signed-off-by: Ludovic Barre <ludovic.barre@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/363982

--- platform/ext/target/stm/common/stm32mp2/tools/flasher/flash_nor.scr.cmd
+++ platform/ext/target/stm/common/stm32mp2/tools/flasher/flash_nor.scr.cmd
@@ -3,35 +3,57 @@
 #
 setenv bl2_file bl2.stm32
 setenv tfm_file tfm_s_ns_signed.bin
-setenv bl2_offset   0x0
-setenv slot0_offset 0x20000
-setenv slot2_offset 0x120000
+setenv fwddr_file ddr4_pmu_train.bin
+
+setenv bl2_p_offset	0x0000000
+setenv bl2_s_offset	0x0040000
+setenv fwddr_p_offset	0x0080000
+setenv fwddr_s_offset	0x00C0000
+setenv tfm_p_offset	0x0100000
+setenv tfm_s_offset	0x0A00000
+setenv tfm_ps_offset	0x1300000
+
 setenv ddr_tmp1 0x90000000
-setenv ddr_tmp2 0x91000000
+setenv ddr_tmp2 0x92000000
 
 setenv cmp_nor 'if cmp.b ${ddr_tmp1} ${ddr_tmp2} ${filesize}; then echo =>OK; else echo =>ERROR; exit ;fi'
 
-echo ===== erase bl2, primary secondary slot =====
-mtd erase nor1 ${bl2_offset} 0x220000
+echo ===== erase bl2, fw_ddr, primary secondary slot =====
+mtd erase nor1 ${bl2_p_offset} 0x1300000
 
 echo ===== write ${bl2_file} =====
-ext4load mmc 0:8 ${ddr_tmp1}  ${bl2_file}
-mtd write nor1 ${ddr_tmp1} ${bl2_offset} ${filesize}
+ext4load mmc 0:8 ${ddr_tmp1} ${bl2_file}
+mtd write nor1 ${ddr_tmp1} ${bl2_p_offset} ${filesize}
+mtd write nor1 ${ddr_tmp1} ${bl2_s_offset} ${filesize}
+
+echo ===== write ${fwddr_file} =====
+ext4load mmc 0:8 ${ddr_tmp1} ${fwddr_file}
+mtd write nor1 ${ddr_tmp1} ${fwddr_p_offset} ${filesize}
+mtd write nor1 ${ddr_tmp1} ${fwddr_s_offset} ${filesize}
 
 echo ===== write ${tfm_file} =====
 ext4load mmc 0:8 ${ddr_tmp1} ${tfm_file}
-mtd write nor1 ${ddr_tmp1} ${slot0_offset} ${filesize}
-mtd write nor1 ${ddr_tmp1} ${slot2_offset} ${filesize}
+mtd write nor1 ${ddr_tmp1} ${tfm_p_offset} ${filesize}
+mtd write nor1 ${ddr_tmp1} ${tfm_s_offset} ${filesize}
 
 echo ===== Check ${tfm_file} =====
-mtd read nor1 ${ddr_tmp2} ${slot0_offset} ${filesize}
+mtd read nor1 ${ddr_tmp2} ${tfm_p_offset} ${filesize}
 run cmp_nor
-mtd read nor1 ${ddr_tmp2} ${slot2_offset} ${filesize}
+mtd read nor1 ${ddr_tmp2} ${tfm_s_offset} ${filesize}
 run cmp_nor
 
-echo ===== Check bl2.stm32 =====
+echo ===== Check ${bl2_file} =====
 ext4load mmc 0:8 ${ddr_tmp1} ${bl2_file}
-mtd read nor1 ${ddr_tmp2} ${bl2_offset} ${filesize}
+mtd read nor1 ${ddr_tmp2} ${bl2_p_offset} ${filesize}
+run cmp_nor
+mtd read nor1 ${ddr_tmp2} ${bl2_s_offset} ${filesize}
+run cmp_nor
+
+echo ===== Check ${fwddr_file} =====
+ext4load mmc 0:8 ${ddr_tmp1} ${fwddr_file}
+mtd read nor1 ${ddr_tmp2} ${fwddr_p_offset} ${filesize}
+run cmp_nor
+mtd read nor1 ${ddr_tmp2} ${fwddr_s_offset} ${filesize}
 run cmp_nor
 
 echo ===== success =====
