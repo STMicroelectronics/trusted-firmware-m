e09edb19147f2fd7ce51d6203c269aa1df96cd07
Michel JAOUEN <michel.jaouen@st.com>
2024-09-13 16:34:49 +0200

    platform: stm32mp2: add a spm file to handle secure interrupt
    tfm_interrupt file is compiled within spm. The file contains
    the secure handler which call spm_handle_interrupt which handles
    the interrupt as FILH or SLIH according to interrupt definition
    within yaml partition definitiion.
    
    Change-Id: I2a378436c46c2cf924fd4abb2eb13a0f7cba2c7b
    Signed-off-by: Michel JAOUEN <michel.jaouen@st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/404456
    Domain-Review: Ludovic BARRE <ludovic.barre@foss.st.com>
    Reviewed-by: Ludovic BARRE <ludovic.barre@foss.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Tested-by: Ludovic BARRE <ludovic.barre@foss.st.com>

---
+++ platform/ext/target/stm/common/stm32mp2/secure/tfm_interrupts.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024, STMicroelectronics. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#include <stdint.h>
+
+#include "cmsis.h"
+#include "spm_ipc.h"
+#include "tfm_hal_interrupt.h"
+#include "tfm_peripherals_def.h"
+#include "ffm/interrupt.h"
+#include "load/interrupt_defs.h"
