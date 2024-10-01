9b4c53c4f83cff29601509dc967b568a6f7ec129
Simeon Marijon <simeon.marijon@foss.st.com>
2024-10-01 15:08:10 +0200

    platform: stm32mp2: improve tfm_plat_otp_read error handling
    outlined by coverity
    
    Signed-off-by: Simeon Marijon <simeon.marijon@foss.st.com>
    Change-Id: I68084efb8ce9396f10356ccd49578da8ccd5bf1b
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/419114
    Tested-by: Michel JAOUEN <michel.jaouen@st.com>
    Reviewed-by: Simeon MARIJON <simeon.marijon@st.com>
    Reviewed-by: Michel JAOUEN <michel.jaouen@st.com>
    Tested-by: Simeon MARIJON <simeon.marijon@st.com>
    Domain-Review: Ludovic BARRE <ludovic.barre@foss.st.com>
    Reviewed-by: Ludovic BARRE <ludovic.barre@foss.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>

--- platform/ext/target/stm/common/stm32mp2xx/secure/provisioning.c
+++ platform/ext/target/stm/common/stm32mp2xx/secure/provisioning.c
@@ -161,8 +161,12 @@ enum tfm_plat_err_t tfm_plat_provisioning_perform(void)
 void tfm_plat_provisioning_check_for_dummy_keys(void)
 {
 	uint64_t iak_start;
+	enum tfm_plat_err_t err;
 
-	tfm_plat_otp_read(PLAT_OTP_ID_IAK, sizeof(iak_start), (uint8_t*)&iak_start);
+	err = tfm_plat_otp_read(PLAT_OTP_ID_IAK, sizeof(iak_start), (uint8_t*)&iak_start);
+	if(err != TFM_PLAT_ERR_SUCCESS) {
+                EMSG("OTP read failed");
+	}
 
 	if(iak_start == 0xA4906F6DB254B4A9) {
 		WMSG("This device was provisioned with dummy keys.\033[1;31m\r\n");
