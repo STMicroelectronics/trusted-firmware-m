d41e3a5bbc803bcc30ee48faaead5ae8853b2531
Ludovic Barre <ludovic.barre@foss.st.com>
2024-12-19 18:40:51 +0100

    platform: stm32mp2: add tfm core peripherals
    Add tfm core peripherals.
    
    Signed-off-by: Ludovic Barre <ludovic.barre@foss.st.com>

---
+++ platform/ext/target/stm/common/stm32mp2xx/tfm_peripherals_def.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2020 STMicroelectronics.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef __TFM_PERIPHERALS_DEF_H__
+#define __TFM_PERIPHERALS_DEF_H__
+
+struct platform_data_t;
+extern const struct platform_data_t tfm_peripheral_timer2;
+
+#define TFM_PERIPHERAL_STD_UART     (0)
+#define TFM_PERIPHERAL_TIMER0       (&tfm_peripheral_timer2)
+#define TFM_TIMER0_IRQ              DT_IRQ(DT_NODELABEL(tim2), irq)
+
+#endif /* __TFM_PERIPHERALS_DEF_H__ */
