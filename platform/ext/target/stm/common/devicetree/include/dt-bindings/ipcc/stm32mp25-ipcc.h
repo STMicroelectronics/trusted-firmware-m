25b92b2f1198526b27d76bb48f25501db186dc8e
Michel JAOUEN <michel.jaouen@st.com>
2025-01-06 18:06:35 +0100

    platform: stm: bindings: add ipcc binding
    This macro is required to define the channel with the same number
    as described in reference manual
    
    Change-Id: I84f130abf909a00ce23373ce59b45c26d9dfc220
    Signed-off-by: Michel JAOUEN <michel.jaouen@st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/427267
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Domain-Review: Ludovic BARRE <ludovic.barre@foss.st.com>
    Reviewed-by: Ludovic BARRE <ludovic.barre@foss.st.com>

---
+++ platform/ext/target/stm/common/devicetree/include/dt-bindings/ipcc/stm32mp25-ipcc.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2025, STMicroelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+#ifndef _DT_BINDINGS_STM32MP25_IPCC_H_
+#define _DT_BINDINGS_STM32MP25_IPCC_H_
+#define IPCC_CHANNEL(x)        (x - 1)
+#endif /* _DT_BINDINGS_STM32MP25_IPCC_H_ */
