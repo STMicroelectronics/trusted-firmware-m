d8e278d17522072e2879e5f30bceae8988927757
Gabriel Fernandez <gabriel.fernandez@foss.st.com>
2025-02-05 16:14:00 +0100

    platform: stm32mp215f_dk: add stm32mp215f Discovery board
    Add support for stm32mp215f Discovery board.
    
    Change-Id: I0427c2a3dc65b549250a85e649cacc25f7984df8
    Signed-off-by: Gabriel Fernandez <gabriel.fernandez@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/434662
    Reviewed-by: Ludovic BARRE <ludovic.barre@foss.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Domain-Review: Ludovic BARRE <ludovic.barre@foss.st.com>

---
+++ platform/ext/target/stm/stm32mp215f_dk/config.cmake
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2020, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-------------------------------------------------------------------------------
+
+# set board specific config
+########################## STM32 #######################################
+#Before Soc config
+
+SET(DTS_BOARD_BASE "arm/stm/stm32mp215f-dk")
+
+# set common soc config
+if (EXISTS ${STM_SOC_DIR}/config.cmake)
+    include(${STM_SOC_DIR}/config.cmake)
+endif()
+
+#After soc config
+set(STM32_BOARD_MODEL           "stm32mp215f dk"        CACHE STRING    "Define board model name" FORCE)
+set(STM32_DDR_PHY_FILE          "lpddr4_pmu_train.bin"  CACHE STRING    "Set ddr phy binary name need for your board" FORCE)
+set(CFG_SCPFW_MOD_POWER_DOMAIN  OFF                     CACHE BOOL      "Disable Power domain modules" FORCE)
