6136efb45d8f6e9acf7e1ec3258ccb9e22f2888a
Michel JAOUEN <michel.jaouen@st.com>
2025-02-18 17:59:53 +0100

    platform: stm32mp2: organise config tfm for soc
    with this set of include, it is possible to have a common config
    for all soc, and to add specific flag for socs, and boards
    
    Change-Id: I9ca67d38d7f28341233282c1a9c2637ff5dff5c8
    Signed-off-by: Michel JAOUEN <michel.jaouen@st.com>
    Signed-off-by: Ludovic Barre <ludovic.barre@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/438762

---
+++ platform/ext/target/stm/common/stm32mp2xx/config_tfm_stm32mp2.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2025, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+#ifndef __CONFIG_TFM_STM32MP2_H__
+#define __CONFIG_TFM_STM32MP2_H__
+
+/* Include optional claims in initial attestation token */
+#undef ATTEST_INCLUDE_OPTIONAL_CLAIMS
+#define ATTEST_INCLUDE_OPTIONAL_CLAIMS	0
+
+/* For size optimization, set CLK_MINIMAL_SZ (no clock name defined e.g.) */
+#ifndef CLK_MINIMAL_SZ
+#define CLK_MINIMAL_SZ	1
+
+#endif
+/* Set ddr4 type */
+#undef STM32MP_DDR4_TYPE
+#define STM32MP_DDR4_TYPE 1
+
+#define SCP_STACK_SIZE 0x2000
+#define IPCC_STACK_SIZE 0x2000
+
+#define CONFIG_TFM_SCHEDULE_WHEN_NS_INTERRUPTED 1
+
+#endif /* __CONFIG_TFM_STM32MP2_H__ */
