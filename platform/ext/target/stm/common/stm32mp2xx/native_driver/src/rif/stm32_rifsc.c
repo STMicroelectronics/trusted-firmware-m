e8ba4bde61b37062fb85b4aa2469435982b42e8d
Raphael Gallais-Pou <raphael.gallais-pou@foss.st.com>
2025-02-18 16:08:47 +0100

    platform: stm32mp2: rif: add hardware semaphore mechanics
    Allow the driver to acquire and release the hardware semaphore for a
    peripheral given a specific configuration.
    
    Change-Id: Ic56473d280d47e8b8beb3ca0d38027cf56ec2eec
    Signed-off-by: Raphael Gallais-Pou <raphael.gallais-pou@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/438521
    Domain-Review: Yann GAUTIER <yann.gautier@foss.st.com>
    Reviewed-by: Ludovic BARRE <ludovic.barre@foss.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>

--- platform/ext/target/stm/common/stm32mp2xx/native_driver/src/rif/stm32_rifsc.c
+++ platform/ext/target/stm/common/stm32mp2xx/native_driver/src/rif/stm32_rifsc.c
@@ -522,10 +522,28 @@ static int stm32_rifsc_firewall_check_access(const struct firewall_spec *spec)
 	return stm32_rifprot_check_access(rifsc_cfg->risup_ctl, rifprot_cfg.id);
 }
 
+static int stm32_rifsc_firewall_acquire_sem(const struct firewall_spec *spec)
+{
+	const struct stm32_rifsc_config *rifsc_cfg = dev_get_config(spec->dev);
+	struct rifprot_config rifprot_cfg = RIFPROT_CFG(spec->args[0]);
+
+	return stm32_rifprot_acquire_sem(rifsc_cfg->risup_ctl, rifprot_cfg.id);
+}
+
+static int stm32_rifsc_firewall_release_sem(const struct firewall_spec *spec)
+{
+	const struct stm32_rifsc_config *rifsc_cfg = dev_get_config(spec->dev);
+	struct rifprot_config rifprot_cfg = RIFPROT_CFG(spec->args[0]);
+
+	return stm32_rifprot_release_sem(rifsc_cfg->risup_ctl, rifprot_cfg.id);
+}
+
 static const struct firewall_controller_api __maybe_unused stm32_rifsc_firewall_api = {
 	.set_conf = stm32_rifsc_firewall_set_conf,
 	.release_conf = stm32_rifsc_firewall_release_conf,
 	.check_access = stm32_rifsc_firewall_check_access,
+	.acquire_access = stm32_rifsc_firewall_acquire_sem,
+	.release_access = stm32_rifsc_firewall_release_sem,
 };
 
 static int __maybe_unused stm32_rifsc_init(const struct device *dev)
