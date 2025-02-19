f3d99039df3b953be0d8933b14a2a60d9b3923bb
Ludovic Barre <ludovic.barre@foss.st.com>
2025-02-19 19:13:37 +0100

    platform: stm32mp257f_ev1: include stm32mp25 config
    include stm32mp25 common config.
    
    Change-Id: I68797c96f24bd1ca36cf15d3845d8c774b385305
    Signed-off-by: Michel JAOUEN <michel.jaouen@st.com>
    Signed-off-by: Ludovic Barre <ludovic.barre@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/438189
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>

--- platform/ext/target/stm/stm32mp257f_ev1/config_tfm_target.h
+++ platform/ext/target/stm/stm32mp257f_ev1/config_tfm_target.h
@@ -7,22 +7,5 @@
 #ifndef __CONFIG_TFM_TARGET_H__
 #define __CONFIG_TFM_TARGET_H__
 
-/* Include optional claims in initial attestation token */
-#undef ATTEST_INCLUDE_OPTIONAL_CLAIMS
-#define ATTEST_INCLUDE_OPTIONAL_CLAIMS	0
-
-/* For size optimization, set CLK_MINIMAL_SZ (no clock name defined e.g.) */
-#ifndef CLK_MINIMAL_SZ
-#define CLK_MINIMAL_SZ	1
-
-#endif
-/* Set ddr4 type */
-#undef STM32MP_DDR4_TYPE
-#define STM32MP_DDR4_TYPE 1
-
-#define SCP_STACK_SIZE 0x2000
-#define IPCC_STACK_SIZE 0x2000
-
-#define CONFIG_TFM_SCHEDULE_WHEN_NS_INTERRUPTED 1
-
+#include <config_tfm_stm32mp25.h>
 #endif /* __CONFIG_TFM_TARGET_H__ */
