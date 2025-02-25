543451ccb2a8dd9b6129e627628be9fcddc65a8a
Yann Gautier <yann.gautier@foss.st.com>
2025-02-25 17:40:07 +0100

    platform: stm32mp2: ddr: align sequences with TF-A
    Align DDR driver code with what is done in TF-A.
    
    Signed-off-by: Yann Gautier <yann.gautier@foss.st.com>
    Change-Id: Iceae4cf3150d8a63791bbddd5a9e47ba94804472
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/439968
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Nicolas LE BAYON <nicolas.le.bayon@st.com>

--- platform/ext/target/stm/common/stm32mp2xx/native_driver/src/ddr/stm32mp2_ddr.c
+++ platform/ext/target/stm/common/stm32mp2xx/native_driver/src/ddr/stm32mp2_ddr.c
@@ -228,7 +228,7 @@ static void ddr_reset(struct stm32mp_ddr_priv *priv)
 
 	udelay(DDR_DELAY_1US);
 
-	//mmio_setbits_32(priv->rcc + RCC_DDRITFCFGR, RCC_DDRITFCFGR_DDRRST);
+	mmio_setbits_32(priv->rcc + RCC_DDRITFCFGR, RCC_DDRITFCFGR_DDRRST);
 	mmio_write_32(priv->rcc + RCC_DDRPHYCAPBCFGR,
 		      RCC_DDRPHYCAPBCFGR_DDRPHYCAPBEN | RCC_DDRPHYCAPBCFGR_DDRPHYCAPBLPEN);
 	mmio_write_32(priv->rcc + RCC_DDRCAPBCFGR,
@@ -242,10 +242,8 @@ static void ddr_standby_reset(struct stm32mp_ddr_priv *priv)
 {
 	udelay(DDR_DELAY_1US);
 
-	mmio_setbits_32(priv->rcc + RCC_DDRCPCFGR, RCC_DDRCPCFGR_DDRCPRST);
-
-//	mmio_write_32(priv->rcc + RCC_DDRCPCFGR,
-//		      RCC_DDRCPCFGR_DDRCPEN | RCC_DDRCPCFGR_DDRCPLPEN | RCC_DDRCPCFGR_DDRCPRST);
+	mmio_write_32(priv->rcc + RCC_DDRCPCFGR,
+		      RCC_DDRCPCFGR_DDRCPEN | RCC_DDRCPCFGR_DDRCPLPEN | RCC_DDRCPCFGR_DDRCPRST);
 	mmio_setbits_32(priv->rcc + RCC_DDRITFCFGR, RCC_DDRITFCFGR_DDRRST);
 	mmio_write_32(priv->rcc + RCC_DDRPHYCAPBCFGR,
 		      RCC_DDRPHYCAPBCFGR_DDRPHYCAPBEN | RCC_DDRPHYCAPBCFGR_DDRPHYCAPBLPEN |
@@ -264,8 +262,7 @@ static void ddr_standby_reset_release(struct stm32mp_ddr_priv *priv)
 {
 	udelay(DDR_DELAY_1US);
 
-//	mmio_write_32(priv->rcc + RCC_DDRCPCFGR, RCC_DDRCPCFGR_DDRCPEN | RCC_DDRCPCFGR_DDRCPLPEN);
-	mmio_clrbits_32(priv->rcc + RCC_DDRCPCFGR, RCC_DDRCPCFGR_DDRCPRST);
+	mmio_write_32(priv->rcc + RCC_DDRCPCFGR, RCC_DDRCPCFGR_DDRCPEN | RCC_DDRCPCFGR_DDRCPLPEN);
 	mmio_clrbits_32(priv->rcc + RCC_DDRITFCFGR, RCC_DDRITFCFGR_DDRRST);
 	mmio_clrbits_32(priv->rcc + RCC_DDRPHYCAPBCFGR, RCC_DDRPHYCAPBCFGR_DDRPHYCAPBRST);
 	mmio_write_32(priv->rcc + RCC_DDRCFGR, RCC_DDRCFGR_DDRCFGEN | RCC_DDRCFGR_DDRCFGLPEN);
