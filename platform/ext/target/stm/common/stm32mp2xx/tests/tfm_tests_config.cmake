b13999bda78c0326cf8fbde90acbcf9d7e0dde60
Ludovic Barre <ludovic.barre@foss.st.com>
2025-02-26 19:33:33 +0100

    platform: stm32mp2: add tests repository
    the multicore service has been re-used to psa service,
    but multicore tests are not applicable.
    
    Change-Id: Ice72acc09119ba90c2a88b3c4a0714b8c51bfb69
    Signed-off-by: Ludovic Barre <ludovic.barre@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/440697
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>

---
+++ platform/ext/target/stm/common/stm32mp2xx/tests/tfm_tests_config.cmake
@@ -0,0 +1,9 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2023, Arm Limited. All rights reserved.
+# Copyright (c) 2025 STMicroelectronics. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-------------------------------------------------------------------------------
+
+set(TEST_NS_MULTI_CORE                  OFF                     CACHE BOOL      "Whether to build NS regression multi-core tests" FORCE)
