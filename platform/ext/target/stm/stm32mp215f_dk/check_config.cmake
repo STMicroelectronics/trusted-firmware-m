b53e7409ed9286c128510fa2f170134f173bcdcf
Ludovic Barre <ludovic.barre@foss.st.com>
2025-03-03 10:01:30 +0100

    platform: stm32mp215f_dk: check invalide configuration
    Check invalid configuration for stm32mp215f_dk board.
    The board has only one sd card, use by mcuboot at bootime and
    leave this storage for cortex A.
    - sdmmc1 is only boot device.
    - no storage available for protected storage.
    
    Change-Id: If16f4a878d0aa33ed79fece071a53c5e0b4cc8e6
    Signed-off-by: Ludovic Barre <ludovic.barre@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/441159
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>

--- platform/ext/target/stm/stm32mp215f_dk/check_config.cmake
+++ platform/ext/target/stm/stm32mp215f_dk/check_config.cmake
@@ -1,3 +1,6 @@
 if (EXISTS ${STM_SOC_DIR}/check_config.cmake)
     include(${STM_SOC_DIR}/check_config.cmake)
 endif()
+
+tfm_invalid_config(NOT STM32_BOOT_DEV STREQUAL "sdmmc1")
+tfm_invalid_config(TFM_PARTITION_PROTECTED_STORAGE)
