b679354113a7f5a1c6af89bf706cd8566f474f59
Nicolas Le Bayon <nicolas.le.bayon@foss.st.com>
2025-03-17 12:04:11 +0100

    platform: stm32mp2: risaf: add MCE support for stm32mp21
    On STM32MP21, RISAF embeds a second encryption method, based on MCE
    peripheral, specifically against side-channel attacks using a specific
    key with two possible sizes: 128 or 256 bits.
    Size can be configured via device tree.
    Add stm32_risaf_install_mce_encryption_key() service in RISAF driver.
    
    Change-Id: I51ec037a045a20ad3454ab485b5740b89f811a25
    Signed-off-by: Nicolas Le Bayon <nicolas.le.bayon@foss.st.com>
    Signed-off-by: Ludovic Barre <ludovic.barre@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/444542
    Domain-Review: Yann GAUTIER <yann.gautier@foss.st.com>
    Tested-by: Gabriel FERNANDEZ <gabriel.fernandez@foss.st.com>
    Reviewed-by: Gabriel FERNANDEZ <gabriel.fernandez@foss.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Nicolas LE BAYON <nicolas.le.bayon@st.com>

--- platform/ext/target/stm/common/stm32mp2xx/native_driver/src/rif/stm32_risaf.c
+++ platform/ext/target/stm/common/stm32mp2xx/native_driver/src/rif/stm32_risaf.c
@@ -25,7 +25,7 @@
 #include <string.h>
 #include <strings.h>
 
-#include <dt-bindings/rif/stm32mp25-risaf.h>
+#include <dt-bindings/rif/stm32mp2-risaf.h>
 
 /* ID Registers */
 #define _RISAF_SR			0x04U
@@ -35,6 +35,11 @@
 #define _RISAF_REG_ENDR			0x48U
 #define _RISAF_REG_CIDCFGR		0x4CU
 #define _RISAF_REGX_OFFSET(x)		(0x40 * (x - 1))
+
+/* RISAF MCE extension registers */
+#define _RISAF_XCR			U(0x1C00)
+#define _RISAF_XSR			U(0x1C04)
+#define _RISAF_MKEYR			U(0x1E00)
 
 #define _RISAF_HWCFGR			0xFF0U
 #define _RISAF_VERR			0xFF4U
@@ -52,8 +57,13 @@
 #define _RISAF_REG_CFGR_BREN		BIT(_RISAF_REG_CFGR_BREN_SHIFT)
 #define _RISAF_REG_CFGR_SEC_SHIFT	U(8)
 #define _RISAF_REG_CFGR_SEC		BIT(_RISAF_REG_CFGR_SEC_SHIFT)
+#ifdef STM32MP21xxxx
+#define _RISAF_REG_CFGR_ENC_SHIFT	U(14)
+#define _RISAF_REG_CFGR_ENC		GENMASK_32(15, 14)
+#else /* STM32MP21xxxx */
 #define _RISAF_REG_CFGR_ENC_SHIFT	U(15)
 #define _RISAF_REG_CFGR_ENC		BIT(_RISAF_REG_CFGR_ENC_SHIFT)
+#endif /* STM32MP21xxxx */
 #define _RISAF_REG_CFGR_PRIVC_SHIFT	U(16)
 #define _RISAF_REG_CFGR_PRIVC_MASK	GENMASK_32(23, 16)
 #define _RISAF_REG_CFGR_ALL_MASK	(_RISAF_REG_CFGR_BREN | \
@@ -75,16 +85,27 @@
 #define _RISAF_REG_CIDCFGR_WRENC_SHIFT	U(16)
 #define _RISAF_REG_CIDCFGR_WRENC_MASK	GENMASK_32(23, 16)
 #define _RISAF_REG_CIDCFGR_ALL_MASK	(_RISAF_REG_CIDCFGR_RDENC_MASK | \
-					 _RISAF_REG_CIDCFGR_WRENC_MASK)
+                                         _RISAF_REG_CIDCFGR_WRENC_MASK)
 
-#define _RISAF_HWCFGR_CFG1_MASK			GENMASK_32(7, 0)
-#define _RISAF_HWCFGR_CFG1_SHIFT		0
-#define _RISAF_HWCFGR_CFG2_MASK			GENMASK_32(15, 8)
-#define _RISAF_HWCFGR_CFG2_SHIFT		8
-#define _RISAF_HWCFGR_CFG3_MASK			GENMASK_32(23, 16)
-#define _RISAF_HWCFGR_CFG3_SHIFT		16
-#define _RISAF_HWCFGR_CFG4_MASK			GENMASK_32(31, 24)
-#define _RISAF_HWCFGR_CFG4_SHIFT		24
+#define _RISAF_HWCFGR_CFG1_MASK		GENMASK_32(7, 0)
+#define _RISAF_HWCFGR_CFG1_SHIFT	0
+#define _RISAF_HWCFGR_CFG2_MASK		GENMASK_32(15, 8)
+#define _RISAF_HWCFGR_CFG2_SHIFT	8
+#define _RISAF_HWCFGR_CFG3_MASK		GENMASK_32(23, 16)
+#define _RISAF_HWCFGR_CFG3_SHIFT	16
+#define _RISAF_HWCFGR_CFG4_MASK		GENMASK_32(31, 24)
+#define _RISAF_HWCFGR_CFG4_SHIFT	24
+
+/* RISAF MCE extension register field description */
+/* _RISAF_XCR register fields */
+#define _RISAF_XCR_XLOCK		BIT(0)
+#define _RISAF_XCR_MKLOCK		BIT(1)
+#define _RISAF_XCR_CIPHERSEL_SHIFT	4
+#define _RISAF_XCR_CIPHERSEL_MASK	GENMASK_32(5, 4)
+#define _RISAF_XCR_CIPHERSEL_AES128	1U
+#define _RISAF_XCR_CIPHERSEL_AES256	3U
+/* _RISAF_XSR register fields */
+#define _RISAF_XSR_MKVALID		BIT(0)
 
 #define _RISAF_GET_REGION_CFG(cfg)					\
 	((_FLD_GET(DT_RISAF_EN, cfg) << _RISAF_REG_CFGR_BREN_SHIFT) |	\
@@ -96,9 +117,19 @@
 	((_FLD_GET(DT_RISAF_WRITE, cfg) << _RISAF_REG_CIDCFGR_WRENC_SHIFT) |	\
 	 (_FLD_GET(DT_RISAF_READ, cfg) << _RISAF_REG_CIDCFGR_RDENC_SHIFT))
 
-#define _RISAF_KEY_SIZE_IN_BYTES		0x10U
-#define _RISAF_TIMEOUT_1MS_IN_US		USEC_PER_MSEC
-#define _RISAF_TIMEOUT_STEP_10US		10U
+#define _RISAF_TIMEOUT_1MS_IN_US	USEC_PER_MSEC
+#define _RISAF_TIMEOUT_100MS_IN_US	USEC_PER_MSEC * 100U
+#define _RISAF_TIMEOUT_STEP_10US	10U
+#define _RISAF_TIMEOUT_STEP_100US	100U
+
+#define BITS_PER_BYTES	8
+
+enum risaf_key_size {
+	RISAF_NO_KEY = 0,
+	RISAF_KEY_128BITS = 128,
+	RISAF_KEY_256BITS = 256,
+	RISAF_MAX_KEY_SZ = 256,
+};
 
 struct risaf_region {
 	uint32_t id;
@@ -114,6 +145,13 @@ struct risaf_dt_region{
 	uint32_t end_addr;
 };
 
+struct stm32_risaf_variant {
+	bool has_enc;
+	int (*default_encryption_fn)(const struct device *dev, uint8_t *key);
+	int (*mce_encryption_fn)(const struct device *dev, uint8_t *key);
+	uint32_t max_key_sz;
+};
+
 struct stm32_risaf_config {
 	uintptr_t base;
 	const struct device *clk_dev;
@@ -121,10 +159,11 @@ struct stm32_risaf_config {
 	const struct risaf_dt_region *dt_regions;
 	const int ndt_regions;
 	const struct device *entropy_dev;
-	const bool has_enc;
+	const uint32_t st_mce_keysize;
 };
 
 struct stm32_risaf_data {
+	const struct stm32_risaf_variant *variant;
 	uint8_t hw_nregions;
 	uint8_t hw_nsubregions;
 	uint8_t hw_granularity;
@@ -211,7 +250,7 @@ static int stm32_risaf_region_cfg(const struct device *dev,
 		return -EINVAL;
 
 	if (region.cfg & _RISAF_REG_CFGR_ENC) {
-		if ((!drv_cfg->has_enc) || !(region.cfg & _RISAF_REG_CFGR_SEC))
+		if ((!drv_data->variant->has_enc) || !(region.cfg & _RISAF_REG_CFGR_SEC))
 			return -EINVAL;
 
 		if (!(mmio_read_32(drv_cfg->base + _RISAF_SR) & _RISAF_SR_KEYVALID) ||
@@ -262,15 +301,15 @@ static bool stm32_risaf_region_need_encryption_key(const struct device *dev)
 	return true;
 }
 
-static int stm32_risaf_install_encryption_key(const struct device *dev,
-					      uint8_t key[_RISAF_KEY_SIZE_IN_BYTES])
+static __unused int stm32_risaf_install_encryption_key(const struct device *dev, uint8_t *key)
 {
 	const struct stm32_risaf_config *drv_cfg = dev_get_config(dev);
+	uint32_t key_size = RISAF_KEY_128BITS;
 	uint64_t sr;
 	uint32_t i;
 	int err;
 
-	for (i = 0U; i < _RISAF_KEY_SIZE_IN_BYTES; i += sizeof(uint32_t)) {
+	for (i = 0U; i < key_size / BITS_PER_BYTES; i += sizeof(uint32_t)) {
 		uint32_t key_val = 0U;
 
 		memcpy(&key_val, key + i, sizeof(uint32_t));
@@ -287,21 +326,71 @@ static int stm32_risaf_install_encryption_key(const struct device *dev,
 	return err;
 }
 
+static __unused int stm32_risaf_install_mce_encryption_key(const struct device *dev, uint8_t *mkey)
+{
+	const struct stm32_risaf_config *drv_cfg = dev_get_config(dev);
+	uint32_t key_size = drv_cfg->st_mce_keysize;
+	uint64_t xsr;
+	uint32_t i;
+	int err;
+
+	if (key_size == RISAF_KEY_128BITS)
+		mmio_write_32(drv_cfg->base + _RISAF_XCR,
+			      _RISAF_XCR_CIPHERSEL_AES128 << _RISAF_XCR_CIPHERSEL_SHIFT);
+	else if (key_size == RISAF_KEY_256BITS)
+		mmio_write_32(drv_cfg->base + _RISAF_XCR,
+			      _RISAF_XCR_CIPHERSEL_AES256 << _RISAF_XCR_CIPHERSEL_SHIFT);
+	else
+		return -EINVAL;
+
+	for (i = 0U; i < key_size / BITS_PER_BYTES; i += sizeof(uint32_t)) {
+		uint32_t key_val = 0U;
+
+		memcpy(&key_val, mkey + i, sizeof(uint32_t));
+                mmio_write_32(drv_cfg->base + _RISAF_MKEYR + i, key_val);
+        }
+
+        err = mmio_read32_poll_timeout(drv_cfg->base + _RISAF_XSR,
+				       xsr,
+				       (xsr & _RISAF_XSR_MKVALID),
+				       _RISAF_TIMEOUT_100MS_IN_US);
+
+	if (err)
+		EMSG("[%s] Timeout waiting mce encryption key expension\n", dev->name);
+
+	return err;
+}
+
 static int stm32_risaf_encryption_init(const struct device *dev)
 {
 	const struct stm32_risaf_config *drv_cfg = dev_get_config(dev);
-	uint8_t key[_RISAF_KEY_SIZE_IN_BYTES];
+	struct stm32_risaf_data *drv_data = dev_get_data(dev);
+	const struct stm32_risaf_variant *variant = drv_data->variant;
+	uint8_t key[RISAF_MAX_KEY_SZ / BITS_PER_BYTES];
 	int err;
 
-	bzero(key, _RISAF_KEY_SIZE_IN_BYTES);
+	if (!variant->default_encryption_fn && !variant->mce_encryption_fn)
+		return -EINVAL;
 
-	err = entropy_get_entropy(drv_cfg->entropy_dev, key, _RISAF_KEY_SIZE_IN_BYTES);
+	bzero(key, RISAF_MAX_KEY_SZ / BITS_PER_BYTES);
+
+	err = entropy_get_entropy(drv_cfg->entropy_dev, key, variant->max_key_sz / BITS_PER_BYTES);
 	if (err) {
 		EMSG("[%s] Could not get specific entropy\n", dev->name);
-		return err;
+		goto out;
 	}
 
-	return stm32_risaf_install_encryption_key(dev, key);
+	if (variant->default_encryption_fn) {
+		err = variant->default_encryption_fn(dev, key);
+		if (err)
+			goto out;
+	}
+
+	if (variant->mce_encryption_fn)
+		err = variant->mce_encryption_fn(dev, key);
+
+out:
+	return err;
 }
 
 static int stm32_risaf_init(const struct device *dev)
@@ -324,7 +413,7 @@ static int stm32_risaf_init(const struct device *dev)
 	if (drv_cfg->ndt_regions > drv_data->hw_nregions)
 		return -EINVAL;
 
-	if (drv_cfg->has_enc && stm32_risaf_region_need_encryption_key(dev)) {
+	if (drv_data->variant->has_enc && stm32_risaf_region_need_encryption_key(dev)) {
 		if (!drv_cfg->entropy_dev)
 			return -EINVAL;
 
@@ -344,6 +433,26 @@ static int stm32_risaf_init(const struct device *dev)
 	return err;
 }
 
+static __unused const struct stm32_risaf_variant stm32mp25_variant = {
+	.has_enc = false,
+	.default_encryption_fn = NULL,
+	.mce_encryption_fn = NULL,
+	.max_key_sz = RISAF_NO_KEY,
+};
+
+static __unused const struct stm32_risaf_variant stm32mp25_enc_variant = {
+	.has_enc = true,
+	.default_encryption_fn = &stm32_risaf_install_encryption_key,
+	.mce_encryption_fn = NULL,
+	.max_key_sz = RISAF_KEY_128BITS,
+};
+
+static __unused const struct stm32_risaf_variant stm32mp21_enc_variant = {
+	.has_enc = true,
+	.default_encryption_fn = &stm32_risaf_install_encryption_key,
+	.mce_encryption_fn = &stm32_risaf_install_mce_encryption_key,
+	.max_key_sz = RISAF_KEY_256BITS,
+};
 
 #define RISAF_ADDR(_n, _mem_region)							\
 	(_mem_region - DT_INST_PROP_BY_IDX(_n, st_mem_map, 1))
@@ -368,35 +477,40 @@ static int stm32_risaf_init(const struct device *dev)
 		.st_protreg = RISAF_MR_DT_PROTREG(_node_id, _prop, _idx),		\
 	},
 
-#define STM32_RISAF_X_INIT(n, variant)							\
+#define STM32_RISAF_INIT(n, name, _variant)						\
 											\
-static const struct risaf_dt_region risaf_dt_regions_##variant####n[] = {		\
+static const struct risaf_dt_region risaf_dt_regions_##name####n[] = {			\
 	DT_INST_FOREACH_PROP_ELEM_SEP_VARGS(n, memory_region, RISAF_REGION, (), n)	\
 };											\
 											\
-static const struct stm32_risaf_config stm32_risaf_cfg_##variant####n = {		\
+static const struct stm32_risaf_config stm32_risaf_cfg_##name####n = {			\
 	.base = DT_INST_REG_ADDR(n),							\
 	.clk_dev = DEVICE_DT_GET(DT_INST_CLOCKS_CTLR(n)),				\
 	.clk_subsys = (clk_subsys_t) DT_INST_CLOCKS_CELL(n, bits),			\
-	.dt_regions = risaf_dt_regions_##variant####n,					\
-	.ndt_regions = ARRAY_SIZE(risaf_dt_regions_##variant####n),			\
+	.dt_regions = risaf_dt_regions_##name####n,					\
+	.ndt_regions = ARRAY_SIZE(risaf_dt_regions_##name####n),			\
 	.entropy_dev = DEVICE_DT_GET_OR_NULL(DT_INST_ENTROPY_CTLR(n)),			\
-	.has_enc = DT_NODE_HAS_COMPAT(DT_DRV_INST(n), st_stm32mp25_risaf_enc),		\
+	.st_mce_keysize = DT_INST_PROP_OR(n, st_mce_keysize_bits,			\
+					  RISAF_KEY_128BITS)				\
 };											\
 											\
-static struct stm32_risaf_data stm32_risaf_data_##variant####n = {};			\
+static struct stm32_risaf_data stm32_risaf_data_##name####n = {				\
+	.variant = &_variant,								\
+};											\
 											\
 DEVICE_DT_INST_DEFINE(n, &stm32_risaf_init,						\
-		      &stm32_risaf_data_##variant####n,					\
-		      &stm32_risaf_cfg_##variant####n,					\
+		      &stm32_risaf_data_##name####n,					\
+		      &stm32_risaf_cfg_##name####n,					\
 		      CORE, 30,								\
 		      NULL);
 
-#define STM32_RISAF_ENC_INIT(n) STM32_RISAF_X_INIT(n, enc)
-#define STM32_RISAF_INIT(n) STM32_RISAF_X_INIT(n, )
-
-DT_INST_FOREACH_STATUS_OKAY(STM32_RISAF_INIT)
+DT_INST_FOREACH_STATUS_OKAY_VARGS(STM32_RISAF_INIT, DT_DRV_COMPAT, stm32mp25_variant)
 
 #undef DT_DRV_COMPAT
 #define DT_DRV_COMPAT st_stm32mp25_risaf_enc
-DT_INST_FOREACH_STATUS_OKAY(STM32_RISAF_ENC_INIT)
+DT_INST_FOREACH_STATUS_OKAY_VARGS(STM32_RISAF_INIT, DT_DRV_COMPAT, stm32mp25_enc_variant)
+
+#undef DT_DRV_COMPAT
+#define DT_DRV_COMPAT st_stm32mp21_risaf_enc
+DT_INST_FOREACH_STATUS_OKAY_VARGS(STM32_RISAF_INIT, DT_DRV_COMPAT, stm32mp21_enc_variant)
+
