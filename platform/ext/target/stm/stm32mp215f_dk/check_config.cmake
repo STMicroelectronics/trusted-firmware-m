b723abb0911d421281922b88caa1e9a84c31a7be
Ludovic Barre <ludovic.barre@foss.st.com>
2025-03-28 16:19:47 +0100

    platform: stm32mp215f_dk: fix check config for ca35tdcid
    In ca35tdcid the tfm is loaded by cortexA in DDR.
    Check on STM32_BOOT_DEV is available only in cm33tdcid.
    
    Change-Id: I3c2bac537113be56a547e6e2eefe2810181e3457
    Signed-off-by: Ludovic Barre <ludovic.barre@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/447534
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>

--- platform/ext/target/stm/stm32mp215f_dk/check_config.cmake
+++ platform/ext/target/stm/stm32mp215f_dk/check_config.cmake
@@ -2,5 +2,8 @@ if (EXISTS ${STM_SOC_DIR}/check_config.cmake)
     include(${STM_SOC_DIR}/check_config.cmake)
 endif()
 
-tfm_invalid_config(NOT STM32_BOOT_DEV STREQUAL "sdmmc1")
+if (${STM32_M33TDCID})
+	tfm_invalid_config(NOT STM32_BOOT_DEV STREQUAL "sdmmc1")
+endif()
+
 tfm_invalid_config(TFM_PARTITION_PROTECTED_STORAGE)
