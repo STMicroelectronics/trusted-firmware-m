STMicroelectronics/trusted-firmware-m

commit 012c3c0a83766ecd95a43b4ebd57c9f4cb4a1ca9

alpha: platform: stm32mp2: fix rproc start stop for debug wrapper
When debug wrapper is used, the cortex a35 is not powered off and
the exti event does not reset the cortex a35, it is required to reset
the cortex a35 to make it starts.

Change-Id: Ia877e6196d57abb51d1ace4698a31b534c619e14
Signed-off-by: Michel JAOUEN <michel.jaouen@st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/422532
Reviewed-by: Ludovic BARRE <ludovic.barre@foss.st.com>
Domain-Review: Francois CHOLEAU <francois.choleau@st.com>
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>

platform/ext/target/stm/common/stm32mp2xx/native_driver/src/remoteproc/stm32_rproc.c
@@ -54,6 +54,14 @@ struct stm32_rproc_data {
  * - apply rif access on exti (exti driver)
  * - send event by exti software interrupt
  */
+static __unused void clr_set_exti_64(void)
+{
+	/*  Clear event if pending */
+	EXTI1->RPR3 = BIT(0);
+	/* send CPU2 SEV event to cpu1 (exti 64)*/
+	EXTI1->SWIER3 |= BIT(0);
+
+}
 
 /*
  * FIXME
@@ -64,6 +72,7 @@ static __unused int stm32mp2_a35_init(const struct device *dev)
 {
 	const struct stm32_rproc_config *cfg = dev_get_config(dev);
 
+	/* unmask event before rif has initialized CID1 filtering on EXTI1_C1CIDCFGR*/
 	EXTI1->C1IMR3 |= BIT(0);
 
 	if (cfg->irq_ack != IRQ_INVALID){
@@ -71,6 +80,12 @@ static __unused int stm32mp2_a35_init(const struct device *dev)
 		EXTI1->RTSR3 |= BIT(1);
 		NVIC_SetPriority(cfg->irq_ack, 1);
 	}
+	/* the deassert release hold boot and set hold boot */
+	reset_control_deassert(&cfg->rst_ctl);
+	/* power up cpu, in case it is in standby*/
+	clr_set_exti_64();
+	/* reset cpu if not in standby */
+	reset_control_assert(&cfg->rst_ctl);
 
 	return 0;
 }
@@ -83,9 +98,13 @@ static __unused int stm32mp2_a35_start(const struct device *dev)
 	struct firewall_spec *firewall;
 	int i, err;
 
-	err = reset_control_assert(&cfg->rst_ctl);
-	if (err)
-		return err;
+	if (PWR_S->CPU1D1SR != PWR_CPU1D1SR_HOLD_BOOT_Msk) {
+		/*  power up cpu, in case it is in standby*/
+		clr_set_exti_64();
+		err = reset_control_assert(&cfg->rst_ctl);
+		if (err)
+			return err;
+	}
 
 	if (cfg->irq_ack != IRQ_INVALID) {
 		/* clear rising pending register */
@@ -114,10 +133,11 @@ static __unused int stm32mp2_a35_start(const struct device *dev)
 	IAC->IER[3] &= ~(IAC_BIT(108));
 	IAC->IER[4] &= ~(IAC_BIT(152) | IAC_BIT(155) | IAC_BIT(156));
 
-	/* send CPU2 SEV event to cpu1 (exti 64)*/
-	EXTI1->SWIER3 |= BIT(0);
+	/*  power up cpu, in case it is in standby */
+	clr_set_exti_64();
+	err = reset_control_deassert(&cfg->rst_ctl);
 
-	return reset_control_deassert(&cfg->rst_ctl);
+	return err;
 }
 
 static __unused int stm32mp2_a35_release(const struct device *dev)
@@ -175,11 +195,16 @@ static __unused int stm32mp2_a35_stop(const struct device *dev)
 	const struct stm32_rproc_config *cfg = dev_get_config(dev);
 	int err;
 
+	/*  power up cpu, in case it is in standby */
+	clr_set_exti_64();
+
 	err = reset_control_assert(&cfg->rst_ctl);
 	if (err)
 		EMSG("[%s] reset err:%d\n", err);
 
-	return stm32mp2_a35_release(dev);
+	err = stm32mp2_a35_release(dev);
+
+	return err;
 }
 
 static __unused void stm32mp2_a35_irq_ack(const struct device *dev)

platform/ext/target/stm/common/stm32mp2xx/native_driver/src/reset/stm32_reset.c
@@ -111,58 +111,42 @@ static const struct reset_driver_api stm32_reset_ops = {
 	.reset = _stm32_reset_reset,
 };
 
-static int _cpu_assert(const struct device *dev, uint32_t id,
-		       unsigned int to_us)
+static int _cpu_assert(const struct device *dev, uint32_t id)
 {
 	const struct stm32_reset_config *drv_cfg = dev_get_config(dev);
 	uintptr_t base = drv_cfg->base;
 	uint32_t rst_offset = RESET_OFFSET(id);
 	uint32_t rst_mask = RESET_BIT(id);
-	uint32_t cpu_mask = CPUBOOT_BIT(rst_offset);
-	uint32_t cfgr;
-	uint32_t no_holdboot;
-	int err = 0;
 
-	no_holdboot = !!(mmio_read_32(base + _RCC_CPUBOOTCR) & cpu_mask);
-	if (!no_holdboot)
-		return 0;
-
-	/* put in HOLD: enable HOLD boot & reset */
-	io_clrbits32(base + _RCC_CPUBOOTCR, cpu_mask);
-	err = mmio_read32_poll_timeout(base + _RCC_CPUBOOTCR,
-				       cfgr, (~cfgr & cpu_mask), to_us);
-	if (err)
-		return err;
 	io_setbits32(base + rst_offset, rst_mask);
 
-	if (!to_us)
-		return 0;
-
-	return mmio_read32_poll_timeout(base + rst_offset, cfgr,
-					(!(cfgr & rst_mask)), to_us);
+	return 0;
 }
 
 static int _cpu_deassert(const struct device *dev, uint32_t id)
 {
 	const struct stm32_reset_config *drv_cfg = dev_get_config(dev);
-	uintptr_t base = drv_cfg->base;
+	uintptr_t addr = drv_cfg->base +_RCC_CPUBOOTCR;
 	uint32_t rst_offset = RESET_OFFSET(id);
 	uint32_t cpu_mask = CPUBOOT_BIT(rst_offset);
-	uint32_t no_holdboot;
-
-	no_holdboot = !!(mmio_read_32(base + _RCC_CPUBOOTCR) & cpu_mask);
-	if (no_holdboot)
-		return -EINVAL;
+	uint32_t cfgr;
+	int err;
 
 	/* release HOLD: disable HOLD boot */
-	io_setbits32(base + _RCC_CPUBOOTCR, cpu_mask);
+	io_setbits32(addr, cpu_mask);
 
-	return 0;
+	/* 1 us timeout to get hold boot not set */
+	err = mmio_read32_poll_timeout(addr, cfgr, (cfgr & cpu_mask), 1);
+
+	/* cpu should start, set hold boot to avoid later an uncontrolled reset */
+	io_clrbits32(addr, cpu_mask);
+
+	return err;
 }
 
 int _stm32_reset_cpu_assert(const struct device *dev, uint32_t id)
 {
-	return _cpu_assert(dev, id, 4 * USEC_PER_MSEC);
+	return _cpu_assert(dev, id);
 }
 
 int _stm32_reset_cpu_deassert(const struct device *dev, uint32_t id)

commit 36023fdc53c496884118f31cb97551ba1f5878c6

platform: stm32mp2: rproc : cleanup EXTI register SWIER3 access
Only set bit in EXTI register when it is not needed to update the value
(read/write) for set bit register (write 0 as no effect).

This patch avoids potential issue for SW interrupt cleared by CA35
during EXTI.SWIER3 update by Cortex M33.

Fixes: 012c3c0a8376 ("alpha: platform: stm32mp2: fix rproc start stop for debug wrapper")
Change-Id: I32af5188fe232b4216239ecda86266ba16c3568c
Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/444571
Reviewed-by: Ludovic BARRE <ludovic.barre@foss.st.com>
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>

platform/ext/target/stm/common/stm32mp2xx/native_driver/src/remoteproc/stm32_rproc.c
@@ -139,7 +139,7 @@ static __unused int stm32mp2_a35_stop(const struct device *dev)
 	/* clear event if pending */
 	EXTI1->RPR3 = BIT(0);
 	/* send CPU2 SEV event to cpu1 (exti 64)*/
-	EXTI1->SWIER3 |= BIT(0);
+	EXTI1->SWIER3 = BIT(0);
 	/* reset cpu */
 	reset_control_assert(&cfg->rst_ctl);
 	/* check cpu in hold boot */