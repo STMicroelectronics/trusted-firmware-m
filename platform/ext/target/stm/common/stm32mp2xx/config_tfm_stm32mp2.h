86a9537db7dfb518c0e973990a668463bc17cbbe
Yann Gautier <yann.gautier@foss.st.com>
2025-04-02 11:53:31 +0200

    platform: stm32mp2: disable CRYPTO_NV_SEED only when M33 is TDCID
    As CRYPTO_HW_ACCELERATOR is only enabled when M33 is TDCID, the flag
    CRYPTO_NV_SEED should be kept to 1 if A35 is TDCID, or else there is
    a compilation error, as none of them are enabled.
    
    Signed-off-by: Yann Gautier <yann.gautier@foss.st.com>
    Change-Id: I6b1eb87a7e1747e941752f5db27cfc238bb2bc49
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/448513
    Reviewed-by: Ludovic BARRE <ludovic.barre@foss.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>

--- platform/ext/target/stm/common/stm32mp2xx/config_tfm_stm32mp2.h
+++ platform/ext/target/stm/common/stm32mp2xx/config_tfm_stm32mp2.h
@@ -36,8 +36,10 @@
 	     psa_get(MAILBOX_SIGNAL_GET_ACTIVE(signals), &msg); \
 	     psa_reply(msg.handle, PSA_SUCCESS); }
 
+#if defined(STM32_M33TDCID)
 /* Use stored NV seed to provide entropy */
 #undef CRYPTO_NV_SEED
 #define CRYPTO_NV_SEED 0
+#endif
 
 #endif /* __CONFIG_TFM_STM32MP2_H__ */
