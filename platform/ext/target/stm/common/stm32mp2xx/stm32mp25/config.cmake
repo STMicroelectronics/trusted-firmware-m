284f893a9cb3a83ab889fbeb6e93208e38283551
Maxime Méré <maxime.mere@foss.st.com>
2025-04-17 15:22:29 +0200

    platform: stm32mp2: remove dummy provisioning by default
    Dummy provisioning should not be enabled by default anymore on STM32MP2
    
    Signed-off-by: Maxime Méré <maxime.mere@foss.st.com>
    Change-Id: I6e35785ef90deba8e7863b0390ad1a147fc9a051
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/451828
    Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Domain-Review: Yann GAUTIER <yann.gautier@foss.st.com>

--- platform/ext/target/stm/common/stm32mp2xx/stm32mp25/config.cmake
+++ platform/ext/target/stm/common/stm32mp2xx/stm32mp25/config.cmake
@@ -18,7 +18,6 @@ set(STM32_STM32MP25_SOC_REV     "revY"          CACHE STRING   "Set soc revision
 
 # set specific board config
 set(STM32_IPC                   ON              CACHE BOOL     "Use IPC (rpmsg) to communicate with main processor" FORCE)
-set(TFM_DUMMY_PROVISIONING      ON              CACHE BOOL     "Provision with dummy values. NOT to be used in production" FORCE)
 set(STM32_PROV_FAKE             ON              CACHE BOOL     "Provisioning with dummy values. NOT to be used in production" FORCE)
 
 set(STM32_HEADER_MAJOR_VER      2               CACHE STRING   "Define stm32 header major version: 2" FORCE)
