7422e9de96eb2cc0b26ffd4399af2bef4f788940
Maxime Méré <maxime.mere@foss.st.com>
2025-05-07 18:05:55 +0200

    platform: stm32mp2: add subkey derivation based on SAES
    Use SAES do derive secret key, as only SAES peripheral can access SoC
    unique secret key.
    
    Use the Key Derivation function (KDF) in counter mode as defined in [1]
    using as the PRF (pseudo random function) based on AES-CMAC.
    PRF(AES-CMAC) is hardware accelerated by SAES, and use the secure DHUK
    (derived hardware unique key) only readable by the SAES peripheral.
    
    Link: https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-108r1.pdf [1]
    
    Signed-off-by: Maxime Méré <maxime.mere@foss.st.com>
    Change-Id: Ia6be2d8eb4fbed8d270179317ae0788f0a6109cb
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/456157
    Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Domain-Review: Yann GAUTIER <yann.gautier@foss.st.com>

--- platform/ext/target/stm/common/stm32mp2xx/secure/crypto_keys.c
+++ platform/ext/target/stm/common/stm32mp2xx/secure/crypto_keys.c
@@ -4,36 +4,313 @@
  * SPDX-License-Identifier: BSD-3-Clause
  *
  */
-
+#include <errno.h>
+#include <lib/utils_def.h>
+#include <sk_cipher.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
-#include "tfm_plat_crypto_keys.h"
-#include "tfm_builtin_key_ids.h"
-#include "tfm_plat_otp.h"
+#include <sys/endian.h>
 #include "psa_manifest/pid.h"
+#include "tfm_builtin_key_ids.h"
 #include "tfm_builtin_key_loader.h"
+#include "tfm_plat_crypto_keys.h"
+#include "tfm_plat_otp.h"
 
 #define NUMBER_OF_ELEMENTS_OF(x) sizeof(x)/sizeof(*x)
 #define MAPPED_TZ_NS_AGENT_DEFAULT_CLIENT_ID -0x3c000000
 #define TFM_NS_PARTITION_ID                  MAPPED_TZ_NS_AGENT_DEFAULT_CLIENT_ID
 
-static enum tfm_plat_err_t tfm_plat_get_huk(uint8_t *buf, size_t buf_len,
-                                            size_t *key_len,
-                                            psa_key_bits_t *key_bits,
-                                            psa_algorithm_t *algorithm,
-                                            psa_key_type_t *type)
+
+#define SAES_NODE_NAME	saes
+#define HUK_SUBKEY_DIE_ID	2
+#define HUK_SUBKEY_MAX_LEN	32
+
+BUILD_ASSERT(DT_NODE_HAS_STATUS(DT_NODELABEL(SAES_NODE_NAME), okay), "saes node is mandatory");
+
+/* HUK subkey management section */
+static void xor_block(uint8_t *b1, uint8_t *b2, size_t size)
 {
-	enum tfm_plat_err_t err;
+	size_t i = 0;
 
-	err = tfm_plat_otp_read(PLAT_OTP_ID_HUK, buf_len, buf);
-	if (err != TFM_PLAT_ERR_SUCCESS) {
-		return err;
+	for (i = 0; i < size; i++)
+		b1[i] ^= b2[i];
+}
+
+static int crypto_keys_cmac_prf_128(const struct device *dev,
+				    enum sk_cipher_key_selection key_sel,
+				    const void *key, size_t key_size,
+				    uint8_t *data, size_t data_size,
+				    uint8_t *out)
+{
+	struct sk_cipher_config_t config;
+	int res = -EPERM;
+	uint8_t block[AES_BLOCK_SIZE] = { };
+	uint8_t k1[AES_BLOCK_SIZE] = { };
+	uint8_t k2[AES_BLOCK_SIZE] = { };
+	uint8_t l[AES_BLOCK_SIZE] = { };
+	size_t processed = 0;
+	uint8_t bit = 0;
+	int i = 0;
+
+	if (!dev)
+		return -ENODEV;
+
+	config = (struct sk_cipher_config_t){
+		.is_dec = false,
+		.ch_mode = SK_CIPHER_MODE_ECB,
+		.key_select = key_sel,
+		.key = key,
+		.key_size = key_size,
+		.iv = NULL,
+		.iv_size = 0,
+	};
+
+	/* Get K1 and K2 */
+	res = sk_cipher_ctx_init(dev, &config);
+	if (res)
+		return res;
+
+	res = sk_cipher_update(dev, true, l, l, sizeof(l));
+	if (res)
+		return res;
+
+	/* MSB(L) == 0 => K1 = L << 1 */
+	bit = 0;
+	for (i = sizeof(l) - 1; i >= 0; i--) {
+		k1[i] = (l[i] << 1) | bit;
+		bit = (l[i] & 0x80) >> 7;
+	}
+	/* MSB(L) == 1 => K1 = (L << 1) XOR const_Rb */
+	if ((l[0] & 0x80))
+		k1[sizeof(k1) - 1] = k1[sizeof(k1) - 1] ^ 0x87;
+
+	/* MSB(K1) == 0 => K2 = K1 << 1 */
+	bit = 0;
+	for (i = sizeof(k1) - 1; i >= 0; i--) {
+		k2[i] = (k1[i] << 1) | bit;
+		bit = (k1[i] & 0x80) >> 7;
 	}
 
-	err = tfm_plat_otp_get_size(PLAT_OTP_ID_HUK, key_len);
-	if (err != TFM_PLAT_ERR_SUCCESS) {
-		return err;
+	/* MSB(K1) == 1 => K2 = (K1 << 1) XOR const_Rb */
+	if ((k1[0] & 0x80))
+		k2[sizeof(k2) - 1] = k2[sizeof(k2) - 1] ^ 0x87;
+
+	if (data_size > AES_BLOCK_SIZE) {
+		uint8_t data_out[AES_BLOCK_SIZE] = {0};
+
+		config = (struct sk_cipher_config_t){
+			.is_dec = false,
+			.ch_mode = SK_CIPHER_MODE_CBC,
+			.key_select = key_sel,
+			.key = key,
+			.key_size = key_size,
+			.iv = block,
+			.iv_size = sizeof(block),
+		};
+
+		/* All block but last in CBC mode */
+		res = sk_cipher_ctx_init(dev, &config);
+		if (res)
+			return res;
+
+		processed = round_down(data_size - 1, AES_BLOCK_SIZE);
+
+		res = sk_cipher_update(dev, true, data, data_out, processed);
+		if (!res) {
+			/* Copy last out block or keep block as { 0 } */
+			memcpy(block, data_out + processed - AES_BLOCK_SIZE,
+			       AES_BLOCK_SIZE);
+		}
+
+		if (res)
+			return res;
 	}
 
+	/* Manage last block */
+	xor_block(block, data + processed, data_size - processed);
+	if (data_size - processed == AES_BLOCK_SIZE) {
+		xor_block(block, k1, AES_BLOCK_SIZE);
+	} else {
+		/* xor with padding = 0b100... */
+		block[data_size - processed] ^= 0x80;
+		xor_block(block, k2, AES_BLOCK_SIZE);
+	}
+
+	/*
+	 * AES last block.
+	 * We need to use same chaining mode to keep same key if DHUK is
+	 * selected so we reuse l as a zero initialized IV.
+	 */
+	memset(l, 0, sizeof(l));
+
+	config = (struct sk_cipher_config_t){
+		.is_dec = false,
+		.ch_mode = SK_CIPHER_MODE_CBC,
+		.key_select = key_sel,
+		.key = key,
+		.key_size = key_size,
+		.iv = l,
+		.iv_size = sizeof(l),
+	};
+
+	res = sk_cipher_ctx_init(dev, &config);
+	if (res)
+		return res;
+
+	return sk_cipher_update(dev, true, block, out, AES_BLOCK_SIZE);
+}
+
+static int crypto_keys_kdf(const struct device *dev,
+			   enum sk_cipher_key_selection key_sel,
+			   const void *key, size_t key_size,
+			   const void *input, size_t input_size,
+			   uint8_t *subkey, size_t subkey_size)
+
+{
+	int res = 0;
+	uint32_t index = 0;
+	uint32_t index_be = 0;
+	uint8_t *data = NULL;
+	size_t data_index = 0;
+	size_t subkey_index = 0;
+	size_t data_size = input_size + sizeof(index_be);
+	uint8_t cmac[AES_BLOCK_SIZE] = { };
+
+	if (!dev || !input || !input_size)
+		return -EINVAL;
+
+	/* For each K(i) we will add an index */
+	data = malloc(data_size);
+	if (!data)
+		return -ENOMEM;
+
+	data_index = 0;
+	index_be = htobe32(index);
+	memcpy(data + data_index, &index_be, sizeof(index_be));
+	data_index += sizeof(index_be);
+	memcpy(data + data_index, input, input_size);
+	data_index += input_size;
+
+	/* K(i) computation. */
+	index = 0;
+	while (subkey_index < subkey_size) {
+		index++;
+		index_be = htobe32(index);
+		memcpy(data, &index_be, sizeof(index_be));
+
+		res = crypto_keys_cmac_prf_128(dev, key_sel, key, key_size,
+					       data, data_size, cmac);
+		if (res)
+			goto out;
+
+		memcpy(subkey + subkey_index, cmac,
+		       MIN(subkey_size - subkey_index, sizeof(cmac)));
+		subkey_index += sizeof(cmac);
+	}
+
+out:
+	free(data);
+	if (res)
+		memset(subkey, 0, subkey_size);
+
+	return res;
+}
+
+/**
+ * @brief Implement hardware HUK derivation using SAES resources
+ * @param dev: derive device instance
+ * @param usage: intended usage of the subkey
+ * @param const_data: constant data to generate different subkeys with the same
+ *		      usage
+ * @param const_data_len: length of constant data
+ * @param subkey: the generated subkey
+ * @param subkey_len: required size of the subkey, sizes larger than
+ *		      HUK_SUBKEY_MAX_LEN are not accepted.
+ *
+ * Returns a subkey derived from the hardware unique key. Given the same
+ * input the same subkey is returned each time.
+ *
+ * @retval 0 on success or an errno code on failure.
+ */
+static int subkey_derive(const struct device *dev, uint32_t usage,
+			 const void *const_data, size_t const_data_len,
+			 uint8_t *subkey, size_t subkey_len)
+{
+	int res = -EPERM;
+	uint8_t *input = NULL;
+	size_t input_index = 0;
+	size_t subkey_bitlen = 0;
+	uint8_t separator = 0;
+
+	if ((!dev) || !device_is_ready(dev))
+		return -ENODEV;
+
+	input = malloc(const_data_len + sizeof(separator) + sizeof(usage) +
+		       sizeof(subkey_bitlen) + AES_BLOCK_SIZE);
+	if (!input)
+		return -ENOMEM;
+
+	input_index = 0;
+	if (const_data) {
+		memcpy(input + input_index, const_data, const_data_len);
+		input_index += const_data_len;
+
+		memcpy(input + input_index, &separator, sizeof(separator));
+		input_index += sizeof(separator);
+	}
+
+	memcpy(input + input_index, &usage, sizeof(usage));
+	input_index += sizeof(usage);
+
+	/*
+	 * We should add the subkey_len in bits at end of input.
+	 * And we choose to put in a MSB first uint32_t.
+	 */
+	subkey_bitlen = htobe32(subkey_len * 8);
+	memcpy(input + input_index, &subkey_bitlen, sizeof(subkey_bitlen));
+	input_index += sizeof(subkey_bitlen);
+
+	/*
+	 * We get K(0) to avoid some key control attack
+	 * and store it at end of input.
+	 */
+	res = crypto_keys_cmac_prf_128(dev, STM32_SAES_KEY_DHU, NULL,
+				       AES_KEYSIZE_128,
+				       input, input_index,
+				       input + input_index);
+	if (res)
+		goto out;
+
+	/* We just added K(0) to input */
+	input_index += AES_BLOCK_SIZE;
+
+	res = crypto_keys_kdf(dev, STM32_SAES_KEY_DHU, NULL, AES_KEYSIZE_128,
+			      input, input_index, subkey, subkey_len);
+
+out:
+	free(input);
+	return res;
+}
+/* end of HUK subkey section */
+
+static enum tfm_plat_err_t tfm_plat_get_huk(uint8_t *buf, size_t buf_len,
+					    size_t *key_len,
+					    psa_key_bits_t *key_bits,
+					    psa_algorithm_t *algorithm,
+					    psa_key_type_t *type)
+{
+	/* Try to get the saes driver */
+	const struct device *dev = SK_CIPHER_GET_BY_NODE_LABEL(SAES_NODE_NAME);
+
+	if (buf_len > HUK_SUBKEY_MAX_LEN)
+		buf_len = HUK_SUBKEY_MAX_LEN;
+
+	if (subkey_derive(dev, HUK_SUBKEY_DIE_ID, NULL, 0, buf, buf_len))
+		return TFM_PLAT_ERR_SYSTEM_ERR;
+
+	*key_len = buf_len;
 	*key_bits = *key_len * 8;
 	*algorithm = PSA_ALG_HKDF(PSA_ALG_SHA_256);
 	*type = PSA_KEY_TYPE_DERIVE;
@@ -43,10 +320,10 @@ static enum tfm_plat_err_t tfm_plat_get_huk(uint8_t *buf, size_t buf_len,
 
 #ifdef TFM_PARTITION_INITIAL_ATTESTATION
 static enum tfm_plat_err_t tfm_plat_get_iak(uint8_t *buf, size_t buf_len,
-                                            size_t *key_len,
-                                            psa_key_bits_t *key_bits,
-                                            psa_algorithm_t *algorithm,
-                                            psa_key_type_t *type)
+					    size_t *key_len,
+					    psa_key_bits_t *key_bits,
+					    psa_algorithm_t *algorithm,
+					    psa_key_type_t *type)
 {
 	enum tfm_plat_err_t err;
 #ifndef SYMMETRIC_INITIAL_ATTESTATION
@@ -124,8 +401,8 @@ static const tfm_plat_builtin_key_policy_t g_builtin_keys_policy[] = {
 
 /**
  * @brief Table describing the builtin-in keys (plaform keys) available in the platform. Note
- *        that to bind the keys to the tfm_builtin_key_loader driver, the lifetime must be
- *        explicitly set to the one associated to the driver, i.e. TFM_BUILTIN_KEY_LOADER_LIFETIME
+ *	  that to bind the keys to the tfm_builtin_key_loader driver, the lifetime must be
+ *	  explicitly set to the one associated to the driver, i.e. TFM_BUILTIN_KEY_LOADER_LIFETIME
  */
 static const tfm_plat_builtin_key_descriptor_t g_builtin_keys_desc[] = {
 	{.key_id = TFM_BUILTIN_KEY_ID_HUK,
