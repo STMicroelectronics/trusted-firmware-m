9d67335eadeae7bbb13f76c438086b16847905d4
Yann Gautier <yann.gautier@foss.st.com>
2025-06-24 17:36:32 +0200

    platform: stm32mp2: silence static_assert warning
    Put the definition of static_assert under a #ifndef, to silence the
    compilation warning.
    
    Signed-off-by: Yann Gautier <yann.gautier@foss.st.com>
    Change-Id: Ibad37cf6727890c9b51ab463e4733703088579d5
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/469980
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>

--- platform/ext/target/stm/common/stm32mp2xx/native_driver/lib/utils_def.h
+++ platform/ext/target/stm/common/stm32mp2xx/native_driver/lib/utils_def.h
@@ -179,8 +179,10 @@
 #define ARG_UNUSED(arg)  ((void)arg)
 #endif
 
+#ifndef static_assert
 #define static_assert(expr, ...) __static_assert(expr, ##__VA_ARGS__, #expr)
 #define __static_assert(expr, msg, ...) _Static_assert(expr, msg)
+#endif
 
 /* Are two types/vars the same type (ignoring qualifiers)? */
 #define __same_type(a, b) __builtin_types_compatible_p(__typeof__(a), __typeof__(b))
