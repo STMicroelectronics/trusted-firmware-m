0b1cd786a180b5d30eac0eb9edf67238cf2a89c8
Amelie Delaunay <amelie.delaunay@foss.st.com>
2025-06-26 09:16:59 +0200

    platform: stm32mp2: add adac and dbgmcu partitions
    An ADAC partition is created, inspired by TF-M-extra PSA-ADAC partition
    example, to handle secure debug request initiated from DBGMCU secure
    mailbox interrupt, managed by additional DBGMCU partition.
    
    Change-Id: I74c9e5703280f1d59896a597b6df16fa0802b29f
    Signed-off-by: Amelie Delaunay <amelie.delaunay@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/469407
    Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Domain-Review: Yann GAUTIER <yann.gautier@foss.st.com>
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>

--- platform/ext/target/stm/common/stm32mp2xx/secure/tfm_interrupts.c
+++ platform/ext/target/stm/common/stm32mp2xx/secure/tfm_interrupts.c
@@ -122,6 +122,21 @@ void RESERVED_285_IRQHandler(void)
 	spm_handle_interrupt(test_sec_npriv_irq.p_pt, test_sec_npriv_irq.p_ildi);
 }
 
+struct irq_t dbg_auth_host_irq = {0};
 
+enum tfm_hal_status_t dbg_auth_host_irq_init(void *p_pt,
+					     const struct irq_load_info_t *p_ildi)
+{
+	dbg_auth_host_irq.p_ildi = p_ildi;
+	dbg_auth_host_irq.p_pt = p_pt;
+	NVIC_SetPriority(p_ildi->source, 1);
+	NVIC_ClearTargetState(p_ildi->source);
+	NVIC_DisableIRQ(p_ildi->source);
 
+	return TFM_HAL_SUCCESS;
+}
 
+void DBG_AUTH_HOST_IRQHandler(void)
+{
+	spm_handle_interrupt(dbg_auth_host_irq.p_pt, dbg_auth_host_irq.p_ildi);
+}
