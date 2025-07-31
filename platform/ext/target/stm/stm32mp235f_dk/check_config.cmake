781da92eae6eac2b0c74bce1641daf4b20bb208f
Yann Gautier <yann.gautier@foss.st.com>
2025-07-31 10:51:14 +0200

    platform: stm32mp235f_dk: check boot device
    This board can only boot from SD-card (on sdmmc1) or eMMC (on sdmmc2).
    It cannot boot on OSPI, as there are no such devices on the board.
    This ospi boot device is then invalid.
    
    Signed-off-by: Yann Gautier <yann.gautier@foss.st.com>
    Change-Id: I1dbc52980b93e675756110b80c6a3d6cd46c6539
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/479423
    Domain-Review: Ludovic BARRE <ludovic.barre@foss.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>

--- platform/ext/target/stm/stm32mp235f_dk/check_config.cmake
+++ platform/ext/target/stm/stm32mp235f_dk/check_config.cmake
@@ -1,3 +1,7 @@
 if (EXISTS ${STM_SOC_DIR}/check_config.cmake)
     include(${STM_SOC_DIR}/check_config.cmake)
 endif()
+
+if (${STM32_M33TDCID})
+    tfm_invalid_config(STM32_BOOT_DEV STREQUAL "ospi")
+endif()
