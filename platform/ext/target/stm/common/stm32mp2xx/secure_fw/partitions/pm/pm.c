fec3f5793cedd846c8b0c6c880a5d43524a7ce47
Ludovic Barre <ludovic.barre@foss.st.com>
2025-08-06 15:49:00 +0200

    platform: stm32mp2: add low power firmware loading
    Loads low-power firmware at power partition init.
    This firmware is stored in specific section of tfm code
    and copy to start of retram.
    
    Change-Id: I1bbeecd0fbc372c6508b2d696d3316757bd9f350
    Signed-off-by: Ludovic Barre <ludovic.barre@foss.st.com>
    Signed-off-by: Simeon Marijon <simeon.marijon@foss.st.com>
    Signed-off-by: Raphael Gallais-Pou <raphael.gallais-pou@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/481722
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>

--- platform/ext/target/stm/common/stm32mp2xx/secure_fw/partitions/pm/pm.c
+++ platform/ext/target/stm/common/stm32mp2xx/secure_fw/partitions/pm/pm.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  *
  */
+#include <cmsis.h>
 #include <errno.h>
 #include <pm/device.h>
 #include <pm/pm.h>
@@ -14,9 +15,74 @@
 
 #define STM32_PM_HINT PM_HINT_POWER_STATE | PM_HINT_CLOCK_STATE | PM_HINT_IO_STATE
 
+extern const uint8_t __tfm_lp_fw_start[];
+
+typedef struct context {
+	uint32_t VTOR;
+	uint32_t MSPLIM;
+	uint32_t PSPLIM;
+	uint32_t CONTROL;
+	uint32_t FPSCR;
+	uint32_t MSP;
+	uint32_t PSP;
+	uint32_t primask_s;
+} cm33_context_t;
+
+cm33_context_t tfm_context;
+
+typedef void (*p_fw_fn)(void);
+
+void save_it_status(void)
+{
+	tfm_context.primask_s = __get_PRIMASK();
+	tfm_context.VTOR = SCB->VTOR;
+	__set_PRIMASK(1);
+}
+
+void restore_it_status(void)
+{
+	SCB->VTOR = tfm_context.VTOR;
+	__DSB();
+	__ISB();
+
+	__set_PRIMASK(tfm_context.primask_s);
+}
+
+void jump_low_power_fw(enum pm_suspend_mode_t lpmode)
+{
+	p_fw_fn lp_fw_entry;
+
+	save_it_status();
+
+	/* save tfm execution context */
+	tfm_context.MSPLIM = __get_MSPLIM();
+	tfm_context.PSPLIM = __get_PSPLIM();
+	tfm_context.PSP = __get_PSP();
+	tfm_context.MSP = __get_MSP();
+	tfm_context.CONTROL = __get_CONTROL();
+	tfm_context.FPSCR = __get_FPSCR();
+
+	lp_fw_entry = (p_fw_fn) *((uint32_t *)(__tfm_lp_fw_start + 4));
+	lp_fw_entry();
+
+	/* restore tfm execution context */
+	__set_MSPLIM(tfm_context.MSPLIM);
+	__set_PSPLIM(tfm_context.PSPLIM);
+	__set_PSP(tfm_context.PSP);
+	__set_MSP(tfm_context.MSP);
+	__set_CONTROL(tfm_context.CONTROL);
+	__set_FPSCR(tfm_context.FPSCR);
+
+	restore_it_status();
+}
+
 static int _pm_suspend(enum pm_suspend_mode_t mode)
 {
 	/* prepare suspend context*/
+
+	/* IT: mask, wakeup, fault ?*/
+	/* cache ? */
+	/* mpu ? */
 
 	/* call suspend of each device */
 	if (IS_ENABLED(CONFIG_PM_DEVICE)) {
@@ -26,7 +92,10 @@ static int _pm_suspend(enum pm_suspend_mode_t mode)
 		}
 	}
 
-	/* core suspend */
+	/* jump_lp_fw */
+	jump_low_power_fw(mode);
+
+	pm_resume_devices(STM32_PM_HINT);
 
 	return 0;
 }
--- platform/ext/target/stm/common/stm32mp2xx/secure_fw/partitions/pm/pm_init.c
+++ platform/ext/target/stm/common/stm32mp2xx/secure_fw/partitions/pm/pm_init.c
@@ -3,16 +3,88 @@
  * SPDX-License-Identifier: BSD-3-Clause
  *
  */
+#include <cmsis.h>
+#include <device.h>
+#include <lib/utils_def.h>
 #include <psa/error.h>
 #include <psa/client.h>
 #include <psa/service.h>
+#include <stm32_dcache.h>
+#include <strings.h>
 #include <tfm_sp_log.h>
 #include <uapi/tfm_pm_api.h>
 
 #include "tfm_pm.h"
 
+extern const uint8_t __tfm_lp_fw_start[];
+extern const uint8_t __tfm_lp_fw_end[];
+
+/*
+ * the CRC computation is calculation follow:
+ * nb RETRAM_BUF_SZ (16K) Starting from RETRAM start address
+ */
+#define RETRAM_BUF_SZ	(16 * 1024)
+
+uint32_t retram_crc_enable(size_t buf_size)
+{
+	uint32_t crcbs = div_round_up(buf_size, RETRAM_BUF_SZ) - 1;
+
+	/* deactivate the CRC */
+	MODIFY_REG(RAMCFG_RETRAM->CCR1, RAMCFG_CCR1_CRCC, 0);
+
+	/* disable hardware erase */
+	SET_BIT(RAMCFG_RETRAM->CR, RAMCFG_CR_SRAMHWERDIS);
+
+	/* Select the buffer size to take into account during the CRC computation */
+	RAMCFG_RETRAM->CCR1 = (crcbs << RAMCFG_CCR1_CRCBS_Pos) |
+		((0x1) << RAMCFG_CCR1_CRCC_Pos);
+
+	/* start the CRC computation */
+	SET_BIT(RAMCFG_RETRAM->CCR2, RAMCFG_CCR2_CRCCS);
+
+	/* wait RETRAM computation is OK */
+	while (RAMCFG_RETRAM->CSR != RAMCFG_CSR_CRCEOC) {}
+
+	/* Clear the CRC status */
+	SET_BIT(RAMCFG_RETRAM->CCR2, RAMCFG_CCR2_CRCFC);
+
+	/* Write 0x0 in CRCC[1:0] bits of the RAMCFG_xCCR1 to deactivate the CRC */
+	MODIFY_REG(RAMCFG_RETRAM->CCR1, RAMCFG_CCR1_CRCC, 0);
+
+	/* Save calculated signature in reference signature */
+	RAMCFG_RETRAM->CRSR = RAMCFG_RETRAM->CCSR;
+
+	/* Enable CRC + Signature check */
+	RAMCFG_RETRAM->CCR1 = (crcbs << RAMCFG_CCR1_CRCBS_Pos) |
+		((0x2) << RAMCFG_CCR1_CRCC_Pos);
+}
+
 static psa_status_t tfm_pm_load_fw()
 {
+	size_t fw_size = __tfm_lp_fw_end - __tfm_lp_fw_start;
+	uintptr_t dst_addr = DT_REG_ADDR(DT_NODELABEL(cm33_retram));
+	size_t dst_sz = DT_REG_SIZE(DT_NODELABEL(cm33_retram));
+	size_t crc_buffer_sz;
+
+	if (!fw_size)
+		return PSA_ERROR_DOES_NOT_EXIST;
+
+	crc_buffer_sz = div_round_up(fw_size, RETRAM_BUF_SZ) * RETRAM_BUF_SZ;
+
+	if (crc_buffer_sz > dst_sz)
+		return PSA_ERROR_GENERIC_ERROR;
+
+	/* set crc_buffer_sz at 0 for computation */
+	bzero((void *)dst_addr, crc_buffer_sz);
+	memcpy((void *)dst_addr, __tfm_lp_fw_start, fw_size);
+
+	__DMB();
+	stm32_dcache_inv((uintptr_t) __tfm_lp_fw_start,
+			 (uintptr_t) (__tfm_lp_fw_start + fw_size));
+
+	/* enable retram crc computation */
+	retram_crc_enable(crc_buffer_sz);
+
 	return PSA_SUCCESS;
 }
 
