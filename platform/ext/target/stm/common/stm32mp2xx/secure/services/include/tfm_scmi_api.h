ff4447e0dc9d11c501fd7d2a20add6a881629520
Michel JAOUEN <michel.jaouen@st.com>
2025-09-02 16:03:07 +0200

    platform: stm32mp2: scp: add notif async for m33 NS
    The aync notif listened by M33 NS agent are generated by scp firmware while
    processing a request from the CA35 Non Secure (linux), or CA35 Secure
    (BL31) or the M33 NS. The scp firmware process of the request is always
    trigered by M33 NS. A buffer to fill async notif if some is passed by NS
    to scp firmware at each request. The NS read the content of the buffer to
    know if an async notif has been generated during scp firmware process.
    
    Change-Id: I7e9335425d320963835cb8d4619e4d7d1365c39a
    Signed-off-by: Michel JAOUEN <michel.jaouen@st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/499880
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>
    Domain-Review: Yann GAUTIER <yann.gautier@foss.st.com>

--- platform/ext/target/stm/common/stm32mp2xx/secure/services/include/tfm_scmi_api.h
+++ platform/ext/target/stm/common/stm32mp2xx/secure/services/include/tfm_scmi_api.h
@@ -5,6 +5,18 @@
 #ifndef TFM_SCMI_API_H
 #define TFM_SCMI_API_H
 #include "psa/client.h"
+/*  psa ret */
+enum psa_scmi_err_t {
+	/* a scmi notif received */
+	PSA_SCMI_ERR_SUCCESS = 0,
+	/* no scmi notif received*/
+	PSA_SCMI_ERR_BUFFER_EMPTY,
+	/* a scmi notif has been received, and at least on scmi notif has been lost */
+	PSA_SCMI_ERR_BUFFER_OVERFLOW,
+};
+
 psa_status_t tfm_scmi_req(void *req, size_t req_len, void *rsp, size_t rsp_len);
 psa_status_t tfm_secure_scmi_req(uint32_t agent_id);
-#endif /* TFM_SCMI_API_H  */
+psa_status_t tfm_secure_scmi_get_notif(void *rsp, size_t rsp_len);
+#endif
+
