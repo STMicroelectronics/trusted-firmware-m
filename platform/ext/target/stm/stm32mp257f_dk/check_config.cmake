1e9f02623dd86126dd760e2567cb66b4f42c37f2
Yann Gautier <yann.gautier@foss.st.com>
2025-10-06 14:31:40 +0200

    platform: stm32mp2: add STM32MP257F-DK board support
    Add platform/ext/target/stm/stm32mp257f_dk folder to support
    STM32MP257F-DK board.
    
    Signed-off-by: Yann Gautier <yann.gautier@foss.st.com>
    Change-Id: I4faaa3004bb4d76546fe98c762f08d4896b2f0d2
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/498332
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>

---
+++ platform/ext/target/stm/stm32mp257f_dk/check_config.cmake
@@ -0,0 +1,9 @@
+if (EXISTS ${STM_SOC_DIR}/check_config.cmake)
+    include(${STM_SOC_DIR}/check_config.cmake)
+endif()
+
+if (${STM32_M33TDCID})
+    tfm_invalid_config(STM32_BOOT_DEV STREQUAL "ospi")
+endif()
+
+tfm_invalid_config(TFM_PARTITION_PROTECTED_STORAGE)
