ce24a0beb897f2ff9177703efb999a0e2e6b2c50
Michel JAOUEN <michel.jaouen@st.com>
2025-10-21 16:24:41 +0200

    platform: stm32mp2: enable ns appli scmi power notification listening
    For system reboot, power down use case m33 ns application should listen
    scmi system power notification. m33 ns sends a request to m33 ns agent to
    activate the listening of scmi notification. When a notification is
    received the function tfm_sys_power_state_notifier is called. This
    function is defined by ns application.
    
    Change-Id: I5a9a633063a5eb138dbc5d41cc9a82290347b930
    Signed-off-by: Michel JAOUEN <michel.jaouen@st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/502318
    Tested-by: Yann GAUTIER <yann.gautier@foss.st.com>
    Domain-Review: Yann GAUTIER <yann.gautier@foss.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>

--- platform/ext/target/stm/common/stm32mp2xx/secure/services/include/tfm_scmi_api.h
+++ platform/ext/target/stm/common/stm32mp2xx/secure/services/include/tfm_scmi_api.h
@@ -15,8 +15,17 @@ enum psa_scmi_err_t {
 	PSA_SCMI_ERR_BUFFER_OVERFLOW,
 };
 
+enum scmi_sys_power {
+	SYS_POWER_SHUTDOWN = 0,
+	SYS_POWER_COLD_RESET = 1,
+	SYS_POWER_WARM_RESET = 2,
+	SYS_POWER_SUSPEND = 4
+};
+
 psa_status_t tfm_scmi_req(void *req, size_t req_len, void *rsp, size_t rsp_len);
 psa_status_t tfm_secure_scmi_req(uint32_t agent_id);
 psa_status_t tfm_secure_scmi_get_notif(void *rsp, size_t rsp_len);
+void tfm_sys_power_state_notifier(uint32_t agent_id, bool graceful, enum scmi_sys_power event);
+
 #endif
 
