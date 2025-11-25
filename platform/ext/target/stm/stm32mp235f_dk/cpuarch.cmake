f9e8227da6b51c1b9ff7a253bf353ef18fef91b5
Yann Gautier <yann.gautier@foss.st.com>
2025-11-25 18:52:20 +0100

    platform: stm32mp235f_dk: overwrite DDR configs and define STM32_SOC_NAME
    To easily pass the correct definition to low-power firmware, overwrite
    the DDR parameters with the proper values for this board.
    Create a STM32_SOC_NAME to pass to low-power firmware, and use it in
    add_compile_definitions rule.
    
    Signed-off-by: Yann Gautier <yann.gautier@foss.st.com>
    Change-Id: Id6754bf0715764f91ee85b6c63e95947583c297a
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/512553
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>

--- platform/ext/target/stm/stm32mp235f_dk/cpuarch.cmake
+++ platform/ext/target/stm/stm32mp235f_dk/cpuarch.cmake
@@ -19,8 +19,10 @@ else()
 set(STM_DIR ${STM_BOARD_DIR}/..)
 endif()
 
+set(STM32_SOC_NAME  STM32MP235Fxx           CACHE STRING    "Define SoC model name" FORCE)
+
 include(${STM_DIR}/common/stm32mp2xx/stm32mp23/cpuarch.cmake)
 
 add_compile_definitions(
-	STM32MP235Fxx
+    ${STM32_SOC_NAME}
 )
