571441d7fc1ad82e4bbd73ad62d8cca767b8a3c6
Maxime Méré <maxime.mere@foss.st.com>
2026-01-05 11:47:40 +0100

    BL2: Add MCUBoot RAM load revert support
    MCUBoot can support revert functionality using the RAM load strategy.
    This commit adds the necessary patches to enable this feature in
    MCUBoot, including updates to CMake and Kconfig configuration options.
    
    Signed-off-by: Maxime Méré <maxime.mere@foss.st.com>
    Change-Id: I3eb1f515eb86f5421a4c566c7d235795ae8655cb
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/522549
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
    Domain-Review: Yann GAUTIER <yann.gautier@foss.st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>

--- bl2/ext/mcuboot/mcuboot_default_config.cmake
+++ bl2/ext/mcuboot/mcuboot_default_config.cmake
@@ -36,6 +36,7 @@ set_property(CACHE MCUBOOT_UPGRADE_STRATEGY PROPERTY STRINGS "OVERWRITE_ONLY;SWA
 set_property(CACHE MCUBOOT_ALIGN_VAL PROPERTY STRINGS "1;2;4;8;16;32")
 
 set(MCUBOOT_DIRECT_XIP_REVERT           ON          CACHE BOOL      "Enable the revert mechanism in direct-xip mode")
+set(MCUBOOT_RAM_LOAD_REVERT             OFF         CACHE BOOL      "Enable the revert mechanism in ram-load mode")
 set(MCUBOOT_HW_ROLLBACK_PROT            ON          CACHE BOOL      "Enable security counter validation against non-volatile HW counters")
 set(MCUBOOT_ENC_IMAGES                  OFF         CACHE BOOL      "Enable encrypted image upgrade support")
 set(MCUBOOT_BOOTSTRAP                   OFF         CACHE BOOL      "Support initial state with empty primary slot and images installed from secondary slots")
