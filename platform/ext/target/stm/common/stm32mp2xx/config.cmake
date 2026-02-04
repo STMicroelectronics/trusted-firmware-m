STMicroelectronics/trusted-firmware-m

commit c6d4bd850a6a8571e7355f2437b3bf128677b716

platform: stm32mp2: reset the system for M33TDCID
The NVIC System reset with AIRCR only reset the M33 sub-system
(CPU2_SYSRESETQ = SYSRESETREQ functional reset generated by AIRCR
core register control bit of the AITR register).

For cortex M33 TDCID, the software system reset from the RCC is generated
when the cortex M33 (CPU2) sets SYSRST in RCC_GRSTCSETR.

Change-Id: I5ed9ad3807cc797b92b9876037c9df121ba276ef
Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/430066
Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
Reviewed-by: Ludovic BARRE <ludovic.barre@foss.st.com>

platform/ext/target/stm/common/stm32mp2xx/secure/services/src/tfm_platform_system.c
@@ -5,18 +5,36 @@
  *
  */
 #include <cmsis.h>
+#include <debug.h>
+#include <reset.h>
 #include <tfm_platform_system.h>
 #include <uapi/tfm_ioctl_api.h>
 #include <tfm_sp_log.h>
 
 #include <cpus.h>
 
+#if defined(STM32MP21xxxx)
+#include <dt-bindings/reset/st,stm32mp21-rcc.h>
+#else
+#include <dt-bindings/reset/st,stm32mp25-rcc.h>
+#endif
+
+/* Resets the system with RCC when TDCID or locally. */
 void tfm_platform_hal_system_reset(void)
 {
-	/* Reset the system */
-	NVIC_SystemReset();
-}
+	struct reset_control rstc;
 
+	if (IS_ENABLED(STM32_M33TDCID)) {
+		rstc.dev = DEVICE_DT_GET(DT_NODELABEL(rcc_reset));
+		rstc.id = SYS_R;
+
+		IMSG("System reset");
+		reset_control_assert(&rstc);
+	} else {
+		IMSG("Cortex-M33 reset");
+		NVIC_SystemReset();
+	}
+}
 enum tfm_platform_err_t tfm_platform_hal_ioctl(tfm_platform_ioctl_req_t request,
 					       psa_invec  *in_vec,
 					       psa_outvec *out_vec)

commit 8f7be7f98c37866929621fa8d1083c97278d0f6d

platform: stm32mp2: reset the system for M33TDCID errors
Use the platform system reset with RCC for tfm_hal_system_reset() used
for all TF-M core errors, as panic function, and not only for the platform
services with tfm_platform_hal_system_reset().

The NVIC System reset with AIRCR only reset the M33 sub-system
(CPU2_SYSRESETQ = SYSRESETREQ functional reset generated by AIRCR
core register control bit of the AITR register).

For cortex M33 TDCID, the software system reset from the RCC is generated
when the cortex M33 (CPU2) sets SYSRST in RCC_GRSTCSETR.

For information, reset_control_assert() can't be used in the function
tfm_hal_system_reset() because this TF-M HAL function be called
before any driver initialization (panic for example), including the RCC
reset controller.

Fixes: c6d4bd850a6a ("platform: stm32mp2: reset the system for M33TDCID")
Change-Id: I0ce001f117d2ef87b55fdae4bf4335ecd3e140ab
Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/498218
Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>
ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
Domain-Review: Yann GAUTIER <yann.gautier@foss.st.com>
Tested-by: Yann GAUTIER <yann.gautier@foss.st.com>

platform/ext/target/stm/common/stm32mp2xx/config.cmake
@@ -42,6 +42,7 @@ set(TFM_PARTITION_PLATFORM              ON          CACHE BOOL  "Enable the TF-M
 set(PLATFORM_DEFAULT_CRYPTO_KEYS        OFF         CACHE BOOL  "Use default crypto keys implementation.")
 set(CRYPTO_HW_ACCELERATOR               ON          CACHE BOOL  "Whether to enable the crypto hardware accelerator on supported platforms")
 set(STM32_PROFILE_DEFINITION		"STM32_PROFILE_MEDIUM"	CACHE STRING	"The default profile definition of platform")
+set(PLATFORM_DEFAULT_SYSTEM_RESET_HALT  OFF         CACHE BOOL  "Use default system reset/halt implementation")
 
 if (STM32_M33TDCID)
 	set(BL2                               ON                   CACHE BOOL   "Whether to build BL2" FORCE)

platform/ext/target/stm/common/stm32mp2xx/secure/services/src/tfm_platform_system.c
@@ -5,8 +5,7 @@
  *
  */
 #include <cmsis.h>
-#include <debug.h>
-#include <reset.h>
+#include <tfm_hal_platform.h>
 #include <tfm_platform_system.h>
 #include <uapi/tfm_ioctl_api.h>
 #include <tfm_sp_log.h>
@@ -15,28 +14,11 @@
 #include <wdt.h>
 #include <stm32_keyshare.h>
 
-#if defined(STM32MP21xxxx)
-#include <dt-bindings/reset/st,stm32mp21-rcc.h>
-#else
-#include <dt-bindings/reset/st,stm32mp25-rcc.h>
-#endif
-
-/* Resets the system with RCC when TDCID or locally. */
 void tfm_platform_hal_system_reset(void)
 {
-	struct reset_control rstc;
-
-	if (IS_ENABLED(STM32_M33TDCID)) {
-		rstc.dev = DEVICE_DT_GET(DT_NODELABEL(rcc_reset));
-		rstc.id = SYS_R;
-
-		IMSG("System reset");
-		reset_control_assert(&rstc);
-	} else {
-		IMSG("Cortex-M33 reset");
-		NVIC_SystemReset();
-	}
+	tfm_hal_system_reset();
 }
+
 enum tfm_platform_err_t tfm_platform_hal_ioctl(tfm_platform_ioctl_req_t request,
 					       psa_invec  *in_vec,
 					       psa_outvec *out_vec)

platform/ext/target/stm/common/stm32mp2xx/secure/tfm_hal_platform.c
@@ -5,12 +5,14 @@
  *
  */
 #include <cmsis.h>
+#include <device.h>
 #include <psa_manifest/pid.h>
 #include <tfm_hal_platform.h>
 #include <tfm_plat_defs.h>
 #include <target_cfg.h>
 #include <region.h>
 #include <init.h>
+#include <lib/mmio.h>
 
 #include <uart_stdout.h>
 #include <stm32_dcache.h>
@@ -97,4 +99,39 @@ uint32_t tfm_hal_check_boot_data_access_policy(int32_t partition_id)
 #endif
 
 	return TFM_HAL_ERROR_GENERIC;
+}
+
+/* Resets the system with RCC when TDCID or reset only cortex M33. */
+#define RCC_GRSTCSETR		0x400
+#define _RCC_GRSTCSETR_SYSRST	BIT(0)
+void tfm_hal_system_reset(void)
+{
+	__disable_irq();
+
+	if (IS_ENABLED(STM32_M33TDCID)) {
+		uintptr_t base = DT_REG_ADDR(DT_NODELABEL(rcc));
+
+		IMSG("System reset\n");
+		io_write32(base + RCC_GRSTCSETR, _RCC_GRSTCSETR_SYSRST);
+	} else {
+		IMSG("Cortex-M33 reset\n");
+		NVIC_SystemReset();
+	}
+}
+
+void tfm_hal_system_halt(void)
+{
+	/*
+	 * Disable IRQs to stop all threads, not just the thread that
+	 * halted the system.
+	 */
+	__disable_irq();
+
+	/*
+	 * Enter sleep to reduce power consumption and do it in a loop in
+	 * case a signal wakes up the CPU.
+	 */
+	while (1) {
+		__WFE();
+	}
 }