92a0f2a04757c90b83ed5730c413c957765d3161
Ludovic Barre <ludovic.barre@foss.st.com>
2026-03-12 11:44:34 +0100

    platform: stm: devicetree: update to manage phandle array
    Update with zephyr method to manage phandle array
    
    Change-Id: I5c3e5f168fb7ccb743c3a1c9a39b0fe9850d6b58
    Signed-off-by: Ludovic Barre <ludovic.barre@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/560939
    Tested-by: Yann GAUTIER <yann.gautier@foss.st.com>
    Domain-Review: Yann GAUTIER <yann.gautier@foss.st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>

--- platform/ext/target/stm/common/stm32mp2xx/native_driver/include/firewall.h
+++ platform/ext/target/stm/common/stm32mp2xx/native_driver/include/firewall.h
@@ -213,45 +213,40 @@ static inline int firewall_release_access(const struct firewall_spec *spec)
 #define _ACCESS_CTRLS_NAME(node_id) \
 	_CONCAT(_access_ctrls, DEVICE_DT_NAME_GET(node_id))
 
-#define _ACCESS_CTRL_IDX_BY_NAME(node_id, name) \
-	DT_PHA_IDX_BY_NAME(node_id, access_controllers, name)
-
 #define _ACCESS_CTRL_ARRAY_NAME(node_id, idx) \
 	_CONCAT(_CONCAT(_ACCESS_CTRLS_NAME(node_id), _array_), idx)
-
-#define _ACCESS_CTRL_ARRAY_NAME_BY_NAME(node_id, name) \
-	_ACCESS_CTRL_ARRAY_NAME(node_id, _ACCESS_CTRL_IDX_BY_NAME(node_id, name))
 
 #define _ACCESS_CTRL_PHA_NAME(node_id, idx) \
 	_CONCAT(_CONCAT(_ACCESS_CTRLS_NAME(node_id), _pha_), idx)
 
-#define _ACCESS_CTRL_PHA_NAME_BY_NAME(node_id, idx) \
-	_ACCESS_CTRL_PHA_NAME(node_id, _ACCESS_CTRL_IDX_BY_NAME(node_id, name))
+#define _ACCESS_CTRL_PHA_ARRAY_BY_IDX(node_id, idx) \
+	DT_FOREACH_PHA_CELL_BY_IDX_SEP(node_id, access_controllers, idx, DT_PHA_BY_IDX, (,))
 
-#define _ACCESS_CTRL_ITEM(idx, node_id)						\
-	{									\
-		.dev = DEVICE_DT_GET(DT_ACCESS_CTRL_PH_BY_IDX(node_id, idx)),	\
-		.args = DT_ACCESS_CTRL_ARRAY_GET(node_id, idx),			\
-		.nargs = DT_ACCESS_CTRL_ARRAY_LEN_BY_IDX(node_id, idx),		\
+#define _ACCESS_CTRL_ITEM(idx, node_id)							\
+	{										\
+		.dev = DEVICE_DT_GET(DT_ACCESS_CTRL_PH_BY_IDX(node_id, idx)),		\
+		.args = DT_ACCESS_CTRL_ARRAY_GET(node_id, idx),				\
+		.nargs = DT_ACCESS_CTRL_ARRAY_LEN_BY_IDX(node_id, idx),			\
 	}
 
-#define _ACCESS_CTRL_ARRAY_DEFINE(idx, node_id)					\
-	COND_CODE_1(DT_PHA_HAS_ARRAY_BY_IDX(node_id, access_controllers, idx),	\
-	(static const uint32_t _ACCESS_CTRL_ARRAY_NAME(node_id, idx)[] =	\
-		DT_PHA_ARRAY_BY_IDX(node_id, access_controllers, idx)), ())
+#define _ACCESS_CTRL_ARRAY_DEFINE(idx, node_id)						\
+	COND_CODE_1(DT_PROP_HAS_IDX(node_id, access_controllers, idx),			\
+	(static const uint32_t _ACCESS_CTRL_ARRAY_NAME(node_id, idx)[] = {		\
+		_ACCESS_CTRL_PHA_ARRAY_BY_IDX(node_id, idx)				\
+	 }), ())
 
 #define _ACCESS_CTRL_SPEC_DEFINE(idx, node_id)						\
 	COND_CODE_1(DT_PROP_HAS_IDX(node_id, access_controllers, idx),			\
 	(static const struct firewall_spec _ACCESS_CTRL_PHA_NAME(node_id, idx) =	\
 			_ACCESS_CTRL_ITEM(idx, node_id)), ())
 
-#define _ACCESS_CTRLS_SPEC_DEFNE(node_id)					\
-	COND_CODE_1(DT_NODE_HAS_PROP(node_id, access_controllers),		\
-	(static const struct firewall_spec _ACCESS_CTRLS_NAME(node_id)[] =	\
-	 {									\
-		LISTIFY(DT_ACCESS_CTRLS_NUM(node_id),				\
-			_ACCESS_CTRL_ITEM,					\
-			(,), node_id)						\
+#define _ACCESS_CTRLS_SPEC_DEFNE(node_id)						\
+	COND_CODE_1(DT_NODE_HAS_PROP(node_id, access_controllers),			\
+	(static const struct firewall_spec _ACCESS_CTRLS_NAME(node_id)[] =		\
+	 {										\
+		LISTIFY(DT_ACCESS_CTRLS_NUM(node_id),					\
+			_ACCESS_CTRL_ITEM,						\
+			(,), node_id)							\
 	 }),())
 
 /**
@@ -264,20 +259,11 @@ static inline int firewall_release_access(const struct firewall_spec *spec)
 #define DT_ACCESS_CTRL_PH_BY_NAME(node_id, name) \
 	DT_PHANDLE_BY_NAME(node_id, access_controllers, name)
 
-#define DT_ACCESS_CTRL_ARRAY_BY_IDX(node_id, idx) \
-	DT_PHA_ARRAY_BY_IDX(node_id, access_controllers, idx)
-
-#define DT_ACCESS_CTRL_ARRAY_BY_NAME(node_id, name) \
-	DT_PHA_ARRAY_BY_NAME(node_id, access_controllers, name)
-
 #define DT_ACCESS_CTRL_ARRAY_LEN_BY_IDX(node_id, idx) \
-	DT_PHA_ARRAY_LEN_BY_IDX(node_id, access_controllers, idx)
+	DT_PHA_NUM_CELLS_BY_IDX(node_id, access_controllers, idx)
 
 #define DT_INST_ACCESS_CTRL_ARRAY_LEN_BY_IDX(inst, idx) \
 	DT_ACCESS_CTRL_ARRAY_LEN_BY_IDX(DT_DRV_INST(inst), idx)
-
-#define DT_ACCESS_CTRL_ARRAY_LEN_BY_NAME(node_id, name) \
-	DT_PHA_ARRAY_LEN_BY_NAME(node_id, access_controllers, name)
 
 #define DT_ACCESS_CTRL_ARRAY_DEFINE(node_id, idx) \
 	_ACCESS_CTRL_ARRAY_DEFINE(idx, node_id)
@@ -285,25 +271,11 @@ static inline int firewall_release_access(const struct firewall_spec *spec)
 #define DT_INST_ACCESS_CTRL_ARRAY_DEFINE(inst, idx) \
 	DT_ACCESS_CTRL_ARRAY_DEFINE(DT_DRV_INST(inst), idx)
 
-#define DT_ACCESS_CTRL_ARRAY_DEFINE_BY_NAME(node_id, name)			\
-	_ACCESS_CTRL_ARRAY_DEFINE(_ACCESS_CTRL_IDX_BY_NAME(node_id, name),	\
-				  node_id)
-
-#define DT_INST_ACCESS_CTRL_ARRAY_DEFINE_BY_NAME(inst, name) \
-	DT_ACCESS_CTRL_ARRAY_DEFINE_BY_NAME(DT_DRV_INST(inst), name)
-
 #define DT_ACCESS_CTRL_ARRAY_GET(node_id, idx) \
 	_ACCESS_CTRL_ARRAY_NAME(node_id, idx)
 
 #define DT_INST_ACCESS_CTRL_ARRAY_GET(inst, idx) \
 	_ACCESS_CTRL_ARRAY_NAME(DT_DRV_INST(inst), idx)
-
-#define DT_ACCESS_CTRL_ARRAY_GET_BY_NAME(node_id, name)				\
-	_ACCESS_CTRL_ARRAY_NAME(node_id,					\
-				_ACCESS_CTRL_IDX_BY_NAME(node_id, name))
-
-#define DT_INST_ACCESS_CTRL_ARRAY_GET_BY_NAME(inst, name)			\
-	DT_ACCESS_CTRL_ARRAY_GET_BY_NAME(DT_DRV_INST(inst), name)
 
 #define DT_ACCESS_CTRL_DEFINE_BY_IDX(node_id, idx) \
 	_ACCESS_CTRL_ARRAY_DEFINE(idx, node_id); \
@@ -312,13 +284,6 @@ static inline int firewall_release_access(const struct firewall_spec *spec)
 #define DT_INST_ACCESS_CTRL_DEFINE_BY_IDX(node_id, idx) \
 	DT_ACCESS_CTRL_DEFINE_BY_IDX(DT_DRV_INST(inst), idx)
 
-#define DT_ACCESS_CTRL_DEFINE_BY_NAME(node_id, name)				\
-	DT_ACCESS_CTRL_DEFINE_BY_IDX(node_id,					\
-				     _ACCESS_CTRL_IDX_BY_NAME(node_id, name))	\
-
-#define DT_INST_ACCESS_CTRL_DEFINE_BY_NAME(inst, name) \
-	DT_ACCESS_CTRL_DEFINE_BY_NAME(DT_DRV_INST(inst), name)
-
 #define DT_ACCESS_CTRL_GET_BY_IDX(node_id, idx)				\
 	COND_CODE_1(DT_PROP_HAS_IDX(node_id, access_controllers, idx),	\
 		    (&_ACCESS_CTRL_PHA_NAME(node_id, idx)),		\
@@ -326,13 +291,6 @@ static inline int firewall_release_access(const struct firewall_spec *spec)
 
 #define DT_INST_ACCESS_CTRL_GET_BY_IDX(inst, idx) \
 	DT_ACCESS_CTRL_GET_BY_IDX(DT_DRV_INST(inst), idx)
-
-#define DT_ACCESS_CTRL_GET_BY_NAME(node_id, name)				\
-	DT_ACCESS_CTRL_GET_BY_IDX(node_id,					\
-				  _ACCESS_CTRL_IDX_BY_NAME(node_id, name))
-
-#define DT_INST_ACCESS_CTRL_GET_BY_NAME(inst, name) \
-	DT_ACCESS_CTRL_GET_BY_NAME(DT_DRV_INST(inst), name)
 
 /**
  * Macros to define and get all references of access-controllers property
