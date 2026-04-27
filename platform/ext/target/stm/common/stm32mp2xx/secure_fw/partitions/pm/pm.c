STMicroelectronics/trusted-firmware-m

commit 94ddf6b6dcfda5a48b149044f4b1800dcd8884da

platform: stm32mp2: pm: support LP firmware trace
With v0.4.0 of Low power firmware, uart used for firmware trace can
be initialized.

Change-Id: I9d944c3cf1092c1fdf96d1ea72fbf36669ea9d82
Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/512837
Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>

platform/ext/target/stm/common/stm32mp2xx/secure_fw/partitions/pm/pm.c
@@ -208,9 +208,26 @@ psa_status_t tfm_pm_power_off(void)
 
 psa_status_t tfm_pm_fw_init(void)
 {
+	const uintptr_t uart_addr = DT_REG_ADDR(DT_CHOSEN(stdout_device));
+
+#if (TFM_PARTITION_LOG_LEVEL < TFM_PARTITION_LOG_LEVEL_INFO)
+	/* Deactivate LP firmware trace */
+	uart_addr = 0x0;
+#endif
+
+	/* Initialize the shared memory */
+	stm32mp2_lp_fw_clear_data();
+
+	/* Display LP version in INIT phase when uart is defined */
+	stm32mp2_lp_fw_set_uart_addr(uart_addr);
+
 	if (jump_low_power_fw(STM32MP2_LP_FW_LPMODE_INIT)) {
 		return PSA_ERROR_GENERIC_ERROR;
 	}
+
+	/* Debug trace in LP firmware only if UART device is not suspended */
+	if (!IS_ENABLED(STM32_CONSOLE_NO_SUSPEND))
+		stm32mp2_lp_fw_set_uart_addr(0x0);
 
 	return PSA_SUCCESS;
 }

commit ea4911b3f0bee1804b35dd7bafcc65643af2e6f5

platform: stm32mp2: pm: fix encryption key support
Call the low power firmware API stm32mp2_lp_fw_set_mkey() after
stm32mp2_lp_fw_clear_data() to no more erase the key in the shared
memory.

This patch fixes adaptation introduced for v0.4.0 of Low power firmware.

Fixes: 94ddf6b6dcfd ("platform: stm32mp2: pm: support LP firmware trace")
Change-Id: I047a449f02956581ad07b7949bb6cf7a6d2a3af5
Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/trusted-firmware-m/+/559579
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>

platform/ext/target/stm/common/stm32mp2xx/secure_fw/partitions/pm/pm.c
@@ -209,7 +209,7 @@ psa_status_t tfm_pm_power_off(void)
 	return PSA_SUCCESS;
 }
 
-psa_status_t tfm_pm_fw_init(void)
+psa_status_t tfm_pm_fw_init(uint8_t *key, uint8_t size)
 {
 	const uintptr_t uart_addr = DT_REG_ADDR(DT_CHOSEN(stdout_device));
 
@@ -223,6 +223,10 @@ psa_status_t tfm_pm_fw_init(void)
 
 	/* Display LP version in INIT phase when uart is defined */
 	stm32mp2_lp_fw_set_uart_addr(uart_addr);
+
+	if (!stm32mp2_lp_fw_set_mkey(key, size)) {
+		return PSA_ERROR_INVALID_ARGUMENT;
+	}
 
 	if (jump_low_power_fw(STM32MP2_LP_FW_LPMODE_INIT)) {
 		return PSA_ERROR_GENERIC_ERROR;

platform/ext/target/stm/common/stm32mp2xx/secure_fw/partitions/pm/pm_init.c
@@ -16,7 +16,6 @@
 #include <tfm_boot_status.h>
 #include <tfm_sp_log.h>
 #include <uapi/tfm_pm_api.h>
-#include <stm32mp2_lp_fw_api.h>
 #include "tfm_pm.h"
 
 extern const uint8_t __tfm_lp_fw_start[];
@@ -89,11 +88,7 @@ static psa_status_t tfm_pm_load_fw()
 	uintptr_t dst_addr = DT_REG_ADDR(DT_NODELABEL(cm33_retram));
 	size_t dst_sz = DT_REG_SIZE(DT_NODELABEL(cm33_retram));
 	size_t crc_buffer_sz;
-	psa_status_t tfm_res;
 	int ret;
-	uint8_t *key;
-	uint8_t size;
-	bool done;
 
 	if (!fw_size)
 		return PSA_ERROR_DOES_NOT_EXIST;
@@ -116,21 +111,7 @@ static psa_status_t tfm_pm_load_fw()
 	if (ret)
 		return PSA_ERROR_GENERIC_ERROR;
 
-	tfm_res = tfm_core_get_boot_data(TLV_MAJOR_PLATFORM,
-					 (struct tfm_boot_data *)&boot_data,
-					 sizeof(boot_data));
-
-	/* Set DDR Encryption key when present in shared data */
-	if (tfm_res == PSA_SUCCESS &&
-	    tlv_extract_data((struct tfm_boot_data *)&boot_data,
-			     SET_TLV_TYPE(TLV_MAJOR_PLATFORM, TLV_PLAT_DDRENCKEY),
-			     &key, &size) == PSA_SUCCESS) {
-		done = stm32mp2_lp_fw_set_mkey(key, size);
-	} else {
-		done = stm32mp2_lp_fw_set_mkey(NULL, 0);
-	}
-
-	return done ? PSA_SUCCESS : PSA_ERROR_GENERIC_ERROR;
+	return PSA_SUCCESS;
 }
 
 psa_status_t tfm_pm_service_sfn(const psa_msg_t *msg)
@@ -150,6 +131,8 @@ psa_status_t tfm_pm_service_sfn(const psa_msg_t *msg)
 psa_status_t tfm_pm_init(void)
 {
 	psa_status_t ret;
+	uint8_t *key;
+	uint8_t size;
 
 	ret = tfm_pm_load_fw();
 	if (ret) {
@@ -157,7 +140,20 @@ psa_status_t tfm_pm_init(void)
 		return ret;
 	}
 
-	ret = tfm_pm_fw_init();
+	ret = tfm_core_get_boot_data(TLV_MAJOR_PLATFORM,
+				     (struct tfm_boot_data *)&boot_data,
+				     sizeof(boot_data));
+	if (!ret) {
+		ret = tlv_extract_data((struct tfm_boot_data *)&boot_data,
+				       SET_TLV_TYPE(TLV_MAJOR_PLATFORM, TLV_PLAT_DDRENCKEY),
+				       &key, &size);
+	}
+	if (ret) {
+		key = NULL;
+		size = 0;
+	}
+
+	ret = tfm_pm_fw_init(key, size);
 	if (ret) {
 		LOG_ERRFMT("[ERR][PM] init failed\r\n");
 	}

platform/ext/target/stm/common/stm32mp2xx/secure_fw/partitions/pm/tfm_pm.h
@@ -7,7 +7,7 @@
 #ifndef  TFM_PARTITION_PM_H
 #define  TFM_PARTITION_PM_H
 
-psa_status_t tfm_pm_fw_init(void);
+psa_status_t tfm_pm_fw_init(uint8_t *key, uint8_t size);
 psa_status_t tfm_pm_suspend(const psa_msg_t *msg);
 psa_status_t tfm_pm_power_off(void);
 